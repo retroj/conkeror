a71c40cb7dfd5f8ebdda64d560a6893736495341
sabetts <sabetts>
2004-10-25 00:46:50 +0000

    conkeror 0.6

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-VERSION=0.5
+VERSION=0.6
 FILES="conkeror conkeror/content conkeror/content/utils.js conkeror/content/url.js conkeror/content/numberedlinks.js conkeror/content/find.js conkeror/content/contents.rdf conkeror/content/conkeror.xul conkeror/content/conkeror.xml conkeror/content/conkeror.js conkeror/content/conkeror.css conkeror/content/commands.js"
 
 zip conkeror-$VERSION.xpi $FILES install.js
