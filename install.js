f8882c4e7056960e2fad1c01d244fe80420b4b8b
sabetts <sabetts>
2004-11-17 16:12:39 +0000

    *** empty log message ***

--- install.js
+++ install.js
@@ -1,8 +1,8 @@
 // customizable behavior
 var shortName = "conkeror";
-var longName = "Conkeror 0.15";
+var longName = "Conkeror 0.16";
 var installMessage = "You will need to restart you browser to use Conkeror.";
-var gVersion = "0.15";
+var gVersion = "0.16";
 var srDest = 3;
 
 // this function verifies disk space in kilobytes
