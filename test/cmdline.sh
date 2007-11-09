bb6a1e8c1bc9bb2759cfb67f8e695c9e6a099350
John Foerch <jjfoerch@earthlink.net>
2007-11-09 13:02:45 -0500

    test framework: split up test suites into separate scripts
    Common functionality for running the command line tests
    is now in test/test.sh.  This file is not executable, because
    it should be sourced by scripts that use it.  There are now
    two such scripts: test/cmdline.sh and test/batch-js.sh

--- test/cmdline.sh
+++ test/cmdline.sh
@@ -5,60 +5,15 @@ if [[ ! -e build.sh || ! -e install.sh ]]; then
     exit 1
 fi
 
-TESTSRUN=0
-TESTSFAILED=0
-
-# suite desc &rest testargs
-function assert-z () {
-    suite="$1"
-    shift
-    desc="$1"
-    shift
-    echo -n "test: $desc ... "
-    let "TESTSRUN = $TESTSRUN + 1"
-    if test -z "$(eval $@)" ; then
-        echo pass
-    else
-        echo fail
-        let "TESTSFAILED = $TESTSFAILED + 1"
-    fi
-}
-
-# suite desc expected &rest testargs
-function assert-equal () {
-    suite="$1" ; shift
-    desc="$1" ; shift
-    expected="$1" ; shift
-    echo -n "test: $desc ... "
-    let "TESTSRUN = $TESTSRUN + 1"
-    if test "$expected" = "$(eval $@)" ; then
-        echo pass
-    else
-        echo fail
-        let "TESTSFAILED = $TESTSFAILED + 1"
-    fi
-}
-
-
-### Setup
-###
-scratch=$(mktemp -d)
-mkdir -p "$scratch/lib" "$scratch/bin"
-./install.sh -build -prefix "$scratch"
-
-## copy the javascript batch suite to the scratch directory.
-cp test/batch-suite.js "$scratch/"
-BATCHSUITEJS="$scratch/batch-suite.js"
-
-## enter the scratch directory
-pushd "$scratch" > /dev/null
+. test/test.sh
+setup
 
 ## produce a javascript file that just dumpln's "Hello, World!"
 TESTSCRIPT1="$scratch/test1.js"
 TESTSCRIPT1OUT="Hello, World"\!
 echo -e "\nconkeror.dumpln (\"$TESTSCRIPT1OUT\");\n" > "$TESTSCRIPT1"
 
-
+enter_scratch_directory
 
 ### Tests
 ###
@@ -84,42 +39,6 @@ assert-equal \
     "$TESTSCRIPT1OUT" \
     "./bin/conkeror -q -batch -l \"$TESTSCRIPT1\" 2>&1"
 
-## batch-suite.js
-##
-##  Now we run javascript suites by batch-loading a test script into
-##  conkeror, and reading its output.  This is fragile because there
-##  has to be agreement between this script and the js script
-##  concerning how test results are reported.
-##
-UNEXPECTEDOUT=0
-while read LINE; do
-    echo "$LINE"
-    let "TESTSRUN = $TESTSRUN + 1"
-    case "$LINE" in
-        *\ ...\ fail)
-            let "TESTSFAILED = $TESTSFAILED + 1" ;;
-        *\ ...\ pass) ;;
-        *) let "UNEXPECTEDOUT = $UNEXPEXECTEDOUT + 1" ;;
-    esac
-done < <(./bin/conkeror -q -batch -l "$BATCHSUITEJS" 2>&1)
 
-
-## gui-xephyr
-##
-# Xephyr :1 &
-# export DISPLAY=:1
-
-
-### Teardown
-###
-popd > /dev/null
-rm -r "$scratch"
-
-echo -n "Summary: $TESTSRUN run, $TESTSFAILED failed"
-
-if [[ "$UNEXPECTEDOUT" -gt 0 ]]; then
-    echo ", $UNEXPECTEDOUT lines of unexpected output"
-fi
-
-echo
+teardown
 
