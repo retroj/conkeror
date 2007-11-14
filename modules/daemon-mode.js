27039245caaf8d23c63b8f527f4e16fa34c94e64
Jeremy Maitin-Shepard <jeremy@jeremyms.com>
2007-11-14 13:18:57 -0500

    daemon-mode: change to use mode.js infrastructure

--- modules/daemon-mode.js
+++ modules/daemon-mode.js
@@ -5,8 +5,7 @@
  *   This mode gets loaded into application scope.
  */
 
-
-var daemon_mode_enabled = false;
+require("mode.js");
 
 var daemon_quit_exits = true;
 
@@ -19,37 +18,25 @@ function daemon_quit_hook_fn () {
         daemon_mode (-1);
 };
 
-function daemon_mode (arg)
-{
-    // null arg means toggle
-    if (! arg)
-        arg = conkeror.daemon_mode_enabled ? -1 : 1;
-    if (arg < 0) {
-        // disable if on.
-        if (conkeror.daemon_mode_enabled) {
-            conkeror.daemon_mode_enabled = false;
-            var appStartup = Cc["@mozilla.org/toolkit/app-startup;1"]
-                .getService(Ci.nsIAppStartup);
-            appStartup.exitLastWindowClosingSurvivalArea ();
-            remove_hook("quit_hook", daemon_quit_hook_fn);
-        }
-    } else {
-        // enable if off
-        if (daemon_mode_enabled == false) {
-            daemon_mode_enabled = true;
-            var appStartup = Components.classes["@mozilla.org/toolkit/app-startup;1"]
-                .getService(Components.interfaces.nsIAppStartup);
-            appStartup.enterLastWindowClosingSurvivalArea ();
-            add_hook("quit_hook", daemon_quit_hook_fn);
-        }
-    }
-}
+define_global_mode("daemon_mode",
+                   function () { // enable
+                       var appStartup = Components.classes["@mozilla.org/toolkit/app-startup;1"]
+                           .getService(Components.interfaces.nsIAppStartup);
+                       appStartup.enterLastWindowClosingSurvivalArea();
+                       add_hook("quit_hook", daemon_quit_hook_fn);
+                   },
+                   function () { // disable
+                       var appStartup = Cc["@mozilla.org/toolkit/app-startup;1"]
+                           .getService(Ci.nsIAppStartup);
+                       appStartup.exitLastWindowClosingSurvivalArea ();
+                       remove_hook("quit_hook", daemon_quit_hook_fn);
+                   });
 
 require_later("command-line.js");
 
 call_after_load("command-line.js", function () {
         command_line_param_handler("daemon", true, function () {
-                daemon_mode(1);
+                daemon_mode(true);
                 var frame = make_frame();
                 frame.setTimeout(function() { frame.close(); }, 0);
             });
