fe6058f76dd8635ec6ce2c862e02426e80fae069
John Foerch <jjfoerch@earthlink.net>
2007-12-02 11:46:08 -0500

    components/commandline.js: whitespace

--- components/commandline.js
+++ components/commandline.js
@@ -8,29 +8,29 @@ const conkeror_cmdline = {
             iid.equals(Components.interfaces.nsIFactory) ||
             iid.equals(Components.interfaces.nsISupports))
             return this;
-        
+
         throw Components.results.NS_ERROR_NO_INTERFACE;
     },
 
     /* nsICommandLineHandler */
     handle : function (cmdline) {
         cmdline.preventDefault = true;
-        
+
         var conkeror = Components.classes["@conkeror.mozdev.org/application;1"]
-        .getService ()
-        .wrappedJSObject;
+            .getService ()
+            .wrappedJSObject;
         conkeror.handle_command_line(cmdline);
     },
-    
+
     /* nsIFactory */
-    
+
     createInstance : function (outer, iid)
     {
         if (outer != null)
             throw Components.results.NS_ERROR_NO_AGGREGATION;
         return this.QueryInterface(iid);
     },
-    
+
     lockFactory : function (lock) { /* no-op */ }
 };
 
@@ -56,47 +56,47 @@ const conkeror_cmdline_module = {
         if (iid.equals(Components.interfaces.nsIModule) ||
             iid.equals(Components.interfaces.nsISupports))
             return this;
-        
+
         throw Components.results.NS_ERROR_NO_INTERFACE;
     },
-    
+
     /* nsIModule */
     getClassObject : function (compMgr, cid, iid)
     {
         if (cid.equals(clh_CID))
             return conkeror_cmdline.QueryInterface(iid);
-        
+
         throw Components.results.NS_ERROR_NOT_REGISTERED;
     },
-    
+
     registerSelf : function (compMgr, fileSpec, location, type)
     {
         compMgr.QueryInterface(Components.interfaces.nsIComponentRegistrar);
-        
+
         compMgr.registerFactoryLocation(clh_CID,
                                         "conkeror_cmdline",
                                         clh_contractID,
                                         fileSpec,
                                         location,
                                         type);
-        
+
         var catMan = Components.classes["@mozilla.org/categorymanager;1"].
         getService(Components.interfaces.nsICategoryManager);
         catMan.addCategoryEntry("command-line-handler",
                                 clh_category,
                                 clh_contractID, true, true);
     },
-    
+
     unregisterSelf : function (compMgr, location, type)
     {
         compMgr.QueryInterface(Components.interfaces.nsIComponentRegistrar);
         compMgr.unregisterFactoryLocation(clh_CID, location);
-        
+
         var catMan = Components.classes["@mozilla.org/categorymanager;1"].
         getService(Components.interfaces.nsICategoryManager);
         catMan.deleteCategoryEntry("command-line-handler", clh_category);
     },
-    
+
     canUnload : function (compMgr)
     {
         return true;
