cd190e4c395e34388933c7e09966688e01db3eb6
Jeremy Maitin-Shepard <jeremy@jeremyms.com>
2007-12-26 11:07:10 -0500

    To avoid conflict in key bindings, change "h" to map to go-forward
    Previously, "F" mapped to go-forward, but this conflicts with the
    mapping of F to browser-element-follow-other-buffer.

--- modules/bindings/default/browser_buffer_normal.js
+++ modules/bindings/default/browser_buffer_normal.js
@@ -44,9 +44,9 @@ define_key(browser_buffer_normal_keymap, "S-down", "cmd_selectLineNext");
 // URL
 define_key(browser_buffer_normal_keymap, "C-x C-f", "find-url"); 
 define_key(browser_buffer_normal_keymap, "u", "go-up");
-define_key(browser_buffer_normal_keymap, "B", "go-back");
-define_key(browser_buffer_normal_keymap, "F", "go-forward");
+define_key(browser_buffer_normal_keymap, "h", "go-forward");
 define_key(browser_buffer_normal_keymap, "l", "go-back");
+define_key(browser_buffer_normal_keymap, "r", "revert-buffer");
 define_key(browser_buffer_normal_keymap, "g", "open-url");
 
 
