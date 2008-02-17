02aa16c7e742812bf8c329655db1474671dcfb23
Jeremy Maitin-Shepard <jeremy@jeremyms.com>
2008-02-17 01:35:19 -0500

    Set pref plugin.expose_full_path to true by default
    Presumably, most Conkeror users will prefer this.

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -8,3 +8,6 @@ pref("ui.key.generalAccessKey",                   0);
 
 /* Show error pages instead of an alert window. */
 pref("browser.xul.error_pages.enabled", true);
+
+/* Surely preferred by most Conkeror users */
+pref("plugin.expose_full_path", true);
