f8955147e8d4f4dce73c08a4bb2f53c73af04f15
Jeremy Maitin-Shepard <jeremy@jeremyms.com>
2008-03-06 19:20:49 -0500

    Add mark-related bindings for the minibuffer

--- modules/bindings/default/minibuffer.js
+++ modules/bindings/default/minibuffer.js
@@ -5,30 +5,35 @@ var minibuffer_base_keymap = new keymap($parent = default_base_keymap);
 
 // {{{ minibuffer_base_keymap
 
-define_key(minibuffer_base_keymap, "C-a", "minibuffer-cmd_beginLine");
-define_key(minibuffer_base_keymap, "C-e", "minibuffer-cmd_endLine");
+define_key(minibuffer_base_keymap, "C-a", "minibuffer-beginning-of-line");
+define_key(minibuffer_base_keymap, "C-e", "minibuffer-end-of-line");
 define_key(minibuffer_base_keymap, "back_space", "minibuffer-cmd_deleteCharBackward");
 define_key(minibuffer_base_keymap, "M-back_space", "minibuffer-cmd_deleteWordBackward");
 define_key(minibuffer_base_keymap, "C-d", "minibuffer-cmd_deleteCharForward");
 define_key(minibuffer_base_keymap, "delete", "minibuffer-cmd_deleteCharForward");
 define_key(minibuffer_base_keymap, "M-d", "minibuffer-cmd_deleteWordForward");
-define_key(minibuffer_base_keymap, "C-b", "minibuffer-cmd_charPrevious");
-define_key(minibuffer_base_keymap, "left", "minibuffer-cmd_charPrevious");
-define_key(minibuffer_base_keymap, "M-b", "minibuffer-cmd_wordPrevious");
-define_key(minibuffer_base_keymap, "C-f", "minibuffer-cmd_charNext");
-define_key(minibuffer_base_keymap, "right", "minibuffer-cmd_charNext");
-define_key(minibuffer_base_keymap, "M-f", "minibuffer-cmd_wordNext");
+define_key(minibuffer_base_keymap, "C-b", "minibuffer-backward-char");
+define_key(minibuffer_base_keymap, "left", "minibuffer-backward-char");
+define_key(minibuffer_base_keymap, "M-b", "minibuffer-backward-word");
+define_key(minibuffer_base_keymap, "C-f", "minibuffer-forward-char");
+define_key(minibuffer_base_keymap, "right", "minibuffer-forward-char");
+define_key(minibuffer_base_keymap, "M-f", "minibuffer-forward-word");
 define_key(minibuffer_base_keymap, "C-y", "minibuffer-cmd_paste");
 define_key(minibuffer_base_keymap, "M-w", "minibuffer-cmd_copy");
 define_key(minibuffer_base_keymap, "C-k", "minibuffer-cmd_deleteToEndOfLine");
 
-define_key(minibuffer_base_keymap, "home", "minibuffer-cmd_beginLine");
-define_key(minibuffer_base_keymap, "end", "minibuffer-cmd_endLine");
+define_key(minibuffer_base_keymap, "home", "minibuffer-beginning-of-line");
+define_key(minibuffer_base_keymap, "end", "minibuffer-end-of-line");
 define_key(minibuffer_base_keymap, "S-home", "minibuffer-cmd_selectBeginLine");
 define_key(minibuffer_base_keymap, "S-end", "minibuffer-cmd_selectEndLine");
+define_key(minibuffer_base_keymap, "S-left", "minibuffer-cmd_selectCharPrevious");
+define_key(minibuffer_base_keymap, "S-right", "minibuffer-cmd_selectCharNext");
 define_key(minibuffer_base_keymap, "C-back_space", "minibuffer-cmd_deleteWordBackward");
 define_key(minibuffer_base_keymap, "C-S-left", "minibuffer-cmd_selectWordPrevious");
 define_key(minibuffer_base_keymap, "C-S-right", "minibuffer-cmd_selectWordNext");
+
+define_key(minibuffer_base_keymap, "C-space", "minibuffer-set-mark");
+
 
 // Nasty keys
 define_key(minibuffer_base_keymap, "C-r", "minibuffer-cmd_redo");
