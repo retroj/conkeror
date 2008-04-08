f3fec87b8e4386cee114753265d104da4782aff5
Will Farrington <wcfarrington@gmail.com>
2008-04-08 13:51:57 -0400

    Add new google-reader page-mode.

--- defaults/preferences/default-modules.js
+++ defaults/preferences/default-modules.js
@@ -9,6 +9,7 @@ pref("conkeror.load.tab-bar", true);
 
 pref("conkeror.load.page-modes/youtube", true);
 pref("conkeror.load.page-modes/reddit", true);
+pref("conkeror.load.page-modes/google-reader", true);
 pref("conkeror.load.page-modes/google-video", true);
 pref("conkeror.load.page-modes/google-search-results", true);
 pref("conkeror.load.page-modes/youporn", true);
---
+++ modules/page-modes/google-reader.js
@@ -0,0 +1,22 @@
+require("content-buffer.js");
+require("bindings/default/content-buffer/normal.js");
+
+var google_reader_keymap = new keymap($parent = content_buffer_normal_keymap);
+
+define_key(google_reader_keymap, "j", null, $fallthrough);
+define_key(google_reader_keymap, "k", null, $fallthrough);
+define_key(google_reader_keymap, "u", null, $fallthrough);
+define_key(google_reader_keymap, "n", null, $fallthrough);
+define_key(google_reader_keymap, "p", null, $fallthrough);
+define_key(google_reader_keymap, "o", null, $fallthrough);
+define_key(google_reader_keymap, "g", null, $fallthrough);
+define_key(google_reader_keymap, "C-c g", "open-url");
+define_key(google_reader_keymap, "r", null, $fallthrough);
+define_key(google_reader_keymap, "C-c r", "reload");
+define_key(google_reader_keymap, "a", null, $fallthrough);
+
+define_page_mode("google_reader_mode", "Google Reader", $enable = function (buffer) {
+    buffer.local_variables.content_buffer_normal_keymap = google_reader_keymap;
+});
+
+auto_mode_list.push([/^https?:\/\/www\.google\.com\/reader\//, google_reader_mode]);
