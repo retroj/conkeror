803334fd49110f30e33ee4ad4ce008d656f32ceb
Martin Dybdal <dybber@dybber.dk>
2008-04-08 15:25:21 -0400

    Page mode for reddit.com

---
+++ modules/page-modes/reddit.js
@@ -0,0 +1,219 @@
+require("content-buffer.js");
+require("bindings/default/content-buffer/normal.js");
+
+/* Initially based on the greasemonkey userscript "Reddit keyboard
+ * shortcuts", found on userscripts.org. But much has changed because of
+ * a reddit-remake.
+ */
+var reddit_highlight_color = "#BFB";
+
+function reddit_toggleHighlight(elem) {
+  if(elem) {
+    if (elem.highlighted) {
+      elem.style.backgroundColor = elem.originalBackgroundColor;
+      elem.highlighted = false;
+    } else {
+      elem.highlighted = true;
+      elem.style.backgroundColor = reddit_highlight_color;
+    }
+  }
+}
+
+/* Sets up the reddit-mode for the given buffer. */
+function reddit_mode_setup(buffer) {
+  var document = buffer.document;
+  if(document.reddit_mode_loaded) return;
+  else document.reddit_mode_loaded = true;
+  siteTable = document.getElementById("siteTable");
+  if (!siteTable) {
+    /* siteTable not found, abort.Tthis happens e.g. when browsing the
+       preferences */
+    return;
+  }
+  // Get all divs that have a id that starts with ‘thingrow’
+  var links = siteTable.getElementsByTagName("div");
+  links = Array.filter(links, function (element) {
+      var start = element.id.substr(0, 12);
+      if (start === "thingrow_t3_") {
+
+        element.articleId  = element.id.substr(12, element.id.length-12);
+        element.highlighted = false;
+        if(element.style.backgroundColor == "")
+            element.originalBackgroundColor = "transparent";
+        else
+            element.originalBackgroundColor = element.style.backgroundColor;
+        return true;
+      }
+      return false;
+    });
+
+  /* Get the last links on the page titled "next" and "prev" */
+  var anchors = document.getElementsByTagName("a");
+  var nextLinks = Array.filter(anchors, function (element) {
+      return element.textContent && "next" === element.textContent;
+    });
+  var previousLinks = Array.filter(anchors, function (element) {
+      return element.textContent && "prev" === element.textContent;
+    });
+  document.redditNextPage = nextLinks[nextLinks.length-1];
+  document.redditPrevPage = previousLinks[previousLinks.length-1];
+
+  if (!links || !links.length) {
+    return;
+  }
+
+  var current = 0;
+  reddit_toggleHighlight(links[current]);
+
+  document.redditCurrent = current;
+  document.redditLinkDivs = links;
+}
+
+/* Scroll the buffer down to the specified element */
+function reddit_showElement(buffer, element) {
+  function getElementY(element) {
+    var offsetY = 0;
+    var parent;
+    for (parent = element; parent; parent = parent.offsetParent) {
+      if (parent.offsetTop) {
+        offsetY += parent.offsetTop;
+      }
+    }
+    return offsetY;
+  }
+
+  if(element) {
+    var position = getElementY(element);
+    var height = buffer.document.body.offsetHeight;
+    var scrollPosition = buffer.top_frame.pageYOffset;
+    if ((height + scrollPosition - position) < 10 || (position - scrollPosition) < 10) {
+      buffer.top_frame.scroll(0, position);
+    }
+  }
+}
+
+/* Go to the next link (eventually the next page).
+ */
+function reddit_next(buffer) {
+  var document = buffer.document;
+  var links = document.redditLinkDivs;
+  var current = document.redditCurrent;
+  if(links !== undefined && current !== undefined) {
+    if (current < (links.length - 1)) {
+      reddit_toggleHighlight(links[current]);
+      current++;
+      reddit_toggleHighlight(links[current]);
+      reddit_showElement(buffer, links[current]);
+    }
+    else if (document.redditNextPage) {
+      buffer.window.content.location.href = document.redditNextPage.href;
+    }
+    document.redditCurrent = current;
+  }
+}
+
+/* Go to the previous link (above). Change to the previous page if we
+ * are at the top.
+ */
+function reddit_prev(buffer) {
+  var document = buffer.document;
+  var links = document.redditLinkDivs;
+  var current = document.redditCurrent;
+  if(links !== undefined && current !== undefined) {
+    if (current > 0) {
+      reddit_toggleHighlight(links[current]);
+      current--;
+      reddit_toggleHighlight(links[current]);
+      reddit_showElement(buffer, links[current]);
+    }
+    else if (document.redditPrevPage) {
+      buffer.window.content.location.href = document.redditPrevPage.href;
+    }
+    document.redditCurrent = current;
+  }
+}
+
+function reddit_getArticleId(buffer) {
+  if(buffer.document.redditLinkDivs !== undefined &&
+     buffer.document.redditCurrent !== undefined)
+    return buffer.document.redditLinkDivs[buffer.document.redditCurrent].articleId;
+  else
+    return false;
+}
+
+function reddit_open(buffer) {
+  var articleId = reddit_getArticleId(buffer);
+  if(articleId)
+    buffer.window.content.location.href = "http://reddit.com/goto?id=" + articleId;
+}
+
+function reddit_open_comments(buffer) {
+  var articleId = reddit_getArticleId(buffer);
+  if(articleId)
+    buffer.window.content.location.href = "http://reddit.com/info/" + articleId + "/comments/";
+}
+
+interactive("reddit-next-link",
+            "Go to the next reddit entry.",
+            function(I) {
+              reddit_next(I.buffer);
+            });
+
+interactive("reddit-prev-link",
+            "Go to the previous reddit entry.",
+            function(I) {
+              reddit_prev(I.buffer);
+            });
+
+interactive("reddit-open-current",
+            "Open the currently selected link on reddit.",
+            function(I) {
+              reddit_open(I.buffer);
+            });
+
+interactive("reddit-open-comments",
+            "Open the comments-page associated with the currently selected reddit-link.",
+            function(I) {
+              reddit_open_comments(I.buffer);
+            });
+
+
+interactive("reddit-vote-down",
+            "Vote the currently selected link down.",
+            function(I) {
+              var articleId = reddit_getArticleId(I.buffer);
+              if(articleId)
+                I.buffer.top_frame.wrappedJSObject.mod("t3_" + articleId, 0);
+            });
+
+interactive("reddit-vote-up",
+            "Vote the currently selected link up.",
+            function(I) {
+              var articleId = reddit_getArticleId(I.buffer);
+              if(articleId)
+                I.buffer.top_frame.wrappedJSObject.mod("t3_" + articleId, 1);
+            });
+
+/* Creating the keymap */
+define_keymap("reddit_keymap", $parent = content_buffer_normal_keymap);
+define_key(reddit_keymap, "j", "reddit-next-link");
+define_key(reddit_keymap, "k", "reddit-prev-link");
+define_key(reddit_keymap, "o", "reddit-open-current");
+define_key(reddit_keymap, "h", "reddit-open-comments"); // c is already in use :(
+define_key(reddit_keymap, ",", "reddit-vote-up");
+define_key(reddit_keymap, ".", "reddit-vote-down");
+
+/* Setting up and tearing down the mode */
+
+function enable_reddit_mode(buffer) {
+  buffer.local_variables.content_buffer_normal_keymap = reddit_keymap;
+  add_hook.call(buffer, "content_buffer_finished_loading_hook", reddit_mode_setup);
+}
+
+function disable_reddit_mode(buffer) {
+  remove_hook.call(buffer, "content_buffer_finished_loading_hook", reddit_mode_setup);
+}
+
+define_page_mode("reddit_mode", "Reddit", $enable = enable_reddit_mode,
+                                          $disable = disable_reddit_mode);
+auto_mode_list.push([/^https?:\/\/([a-zA-Z0-9\-]\.)*reddit\.com\//, reddit_mode]);
