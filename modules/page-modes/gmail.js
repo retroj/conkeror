0d1b56f26bdce4709517d4230e15e96b0533695e
Will Farrington <wcfarrington@gmail.com>
2008-04-08 23:41:58 -0400

    page-modes/gmail.js: Add fall-throughs for more gmail key bindings

--- modules/page-modes/gmail.js
+++ modules/page-modes/gmail.js
@@ -13,16 +13,46 @@ interactive("gmail-label-go",
               gmail_label_go(I.buffer, (yield I.minibuffer.read($prompt = "Go to label: ")));
             });
 
+// Jumping
 define_key(gmail_keymap, "C-c g", "gmail-label-go");
+
+// Threadlist
+define_key(gmail_keymap, "*", null, $fallthrough);
+
+// Navigation
+define_key(gmail_keymap, "u", null, $fallthrough);
 define_key(gmail_keymap, "j", null, $fallthrough);
 define_key(gmail_keymap, "k", null, $fallthrough);
-define_key(gmail_keymap, "u", null, $fallthrough);
+define_key(gmail_keymap, "o", null, $fallthrough);
 define_key(gmail_keymap, "n", null, $fallthrough);
 define_key(gmail_keymap, "p", null, $fallthrough);
+
+// Application
+define_key(gmail_keymap, "c", null, $fallthrough);
+define_key(gmail_keymap, "C-c c", "copy");
+define_key(gmail_keymap, "/", null, $fallthrough);
+define_key(gmail_keymap, "q", null, $fallthrough);
+define_key(gmail_keymap, "?", null, $fallthrough);
+
+// Actions
+define_key(gmail_keymap, "x", null, $fallthrough);
+define_key(gmail_keymap, "C-c x", "shell-command-on-file");
+define_key(gmail_keymap, "s", null, $fallthrough);
+define_key(gmail_keymap, "C-c s", "save");
+define_key(gmail_keymap, "y", null, $fallthrough);
+define_key(gmail_keymap, "e", null, $fallthrough);
+define_key(gmail_keymap, "m", null, $fallthrough);
+define_key(gmail_keymap, "!", null, $fallthrough);
+define_key(gmail_keymap, "#", null, $fallthrough);
 define_key(gmail_keymap, "r", null, $fallthrough);
 define_key(gmail_keymap, "C-c r", "reload");
-define_key(gmail_keymap, "a", null, $fallthrough);
-define_key(gmail_keymap, "?", null, $fallthrough);
+define_key(gmail_keymap, "f", null, $fallthrough);
+define_key(gmail_keymap, "C-c f", "follow");
+define_key(gmail_keymap, "S-n", null, $fallthrough);
+define_key(gmail_keymap, ".", null, $fallthrough);
+define_key(gmail_keymap, "S-i", null, $fallthrough);
+define_key(gmail_keymap, "S-u", null, $fallthrough);
+
 
 define_page_mode("gmail_mode", "GMail", $enable = function (buffer) {
     buffer.local_variables.content_buffer_normal_keymap = gmail_keymap;
