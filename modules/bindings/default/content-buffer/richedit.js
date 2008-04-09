b0e87b119d2d0de133a2c53489fef458c9d3093b
Jeremy Maitin-Shepard <jeremy@jeremyms.com>
2008-04-09 13:54:13 -0400

    Fix typo in richedit keymap

--- modules/bindings/default/content-buffer/richedit.js
+++ modules/bindings/default/content-buffer/richedit.js
@@ -1,20 +1,3 @@
 require("bindings/default/content-buffer/text.js");
 
-define_keymap("content_buffer_richedit_keymap", $parent = content_buffer_text_keymap);
-
-// textarea keys
-define_key(content_buffer_textarea_keymap, "C-n", "forward-line", $category = "Movement");
-define_key(content_buffer_textarea_keymap, "down", "forward-line", $category = "Movement");
-define_key(content_buffer_textarea_keymap, "C-p", "backward-line", $category = "Movement");
-define_key(content_buffer_textarea_keymap, "up", "backward-line", $category = "Movement");
-define_key(content_buffer_textarea_keymap, "M-<", "beginning-of-first-line", $category = "Movement");
-define_key(content_buffer_textarea_keymap, "M->", "end-of-last-line", $category = "Movement");
-define_key(content_buffer_textarea_keymap, "M-v", "backward-page", $category = "Movement");
-define_key(content_buffer_textarea_keymap, "page_up", "backward-page", $category = "Movement");
-
-define_key(content_buffer_textarea_keymap, "C-v", "forward-page", $category = "Movement");
-define_key(content_buffer_textarea_keymap, "page_down", "forward-page", $category = "Movement");
-
-// 101 keys
-define_key(content_buffer_textarea_keymap, "S-page_up", "cmd_selectPageUp", $category = "Selection");
-define_key(content_buffer_textarea_keymap, "S-page_down", "cmd_selectPageDown", $category = "Selection");
+define_keymap("content_buffer_richedit_keymap", $parent = content_buffer_textarea_keymap);
