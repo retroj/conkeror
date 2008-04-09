cb5b1521bd188cec82cbb1dab725e6c169a585ba
Will Farrington <wcfarrington@gmail.com>
2008-04-08 23:28:13 -0400

    page-modes/google-reader.js: Add more fall-throughs for Google reader commands

--- modules/page-modes/google-reader.js
+++ modules/page-modes/google-reader.js
@@ -3,16 +3,44 @@ require("bindings/default/content-buffer/normal.js");
 
 define_keymap("google_reader_keymap", $parent = content_buffer_normal_keymap);
 
+// Help
+define_key(google_reader_keymap, "?", null, $fallthrough);
+
+// Navigation
 define_key(google_reader_keymap, "j", null, $fallthrough);
 define_key(google_reader_keymap, "k", null, $fallthrough);
-define_key(google_reader_keymap, "u", null, $fallthrough);
+define_key(google_reader_keymap, "space", null, $fallthrough);
+define_key(google_reader_keymap, "S-space", null, $fallthrough);
 define_key(google_reader_keymap, "n", null, $fallthrough);
+define_key(google_reader_keymap, "S-n", null, $fallthrough);
 define_key(google_reader_keymap, "p", null, $fallthrough);
-define_key(google_reader_keymap, "o", null, $fallthrough);
+define_key(google_reader_keymap, "S-p", null, $fallthrough);
+define_key(google_reader_keymap, "S-x", null, $fallthrough);
+define_key(google_reader_keymap, "S-o", null, $fallthrough);
+
+// Jumping
 define_key(google_reader_keymap, "g", null, $fallthrough);
 define_key(google_reader_keymap, "C-c g", "open-url");
+
+// Acting
+define_key(google_reader_keymap, "s", null, $fallthrough);
+define_key(google_reader_keymap, "S-s", null, $fallthrough);
+define_key(google_reader_keymap, "C-c s", "save");
+define_key(google_reader_keymap, "v", null, $fallthrough);
+define_key(google_reader_keymap, "t", null, $fallthrough);
+define_key(google_reader_keymap, "C-c t", "follow-top");
+define_key(google_reader_keymap, "m", null, $fallthrough);
+define_key(google_reader_keymap, "o", null, $fallthrough);
+define_key(google_reader_keymap, "S-a", null, $fallthrough);
+define_key(google_reader_keymap, "e", null, $fallthrough);
+
+// Application
 define_key(google_reader_keymap, "r", null, $fallthrough);
 define_key(google_reader_keymap, "C-c r", "reload");
+define_key(google_reader_keymap, "u", null, $fallthrough);
+define_key(google_reader_keymap, "1", null, $fallthrough);
+define_key(google_reader_keymap, "2", null, $fallthrough);
+define_key(google_reader_keymap, "/", null, $fallthrough);
 define_key(google_reader_keymap, "a", null, $fallthrough);
 
 define_page_mode("google_reader_mode", "Google Reader", $enable = function (buffer) {
