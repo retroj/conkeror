375afb7ebcbf0210bd3b59892f2351f6669e7d29
Jeremy Maitin-Shepard <jeremy@jeremyms.com>
2008-04-12 02:58:02 -0400

    google-search-results.js: add fall-throughs for "experimental" keyboard search

--- modules/page-modes/google-search-results.js
+++ modules/page-modes/google-search-results.js
@@ -1,9 +1,25 @@
+require("content-buffer.js");
+require("bindings/default/content-buffer/normal.js");
+
+define_keymap("google_search_results_keymap", $parent = content_buffer_normal_keymap);
+
+// Keys for the "experimental" keyboard search
+define_key(google_search_results_keymap, "j", null, $fallthrough);
+define_key(google_search_results_keymap, "k", null, $fallthrough);
+define_key(google_search_results_keymap, "o", null, $fallthrough);
+define_key(google_search_results_keymap, "/", null, $fallthrough);
+
+/**
+ * Note: return already falls through by default in the content_buffer_normal_keymap,
+ * and escape already does the same thing as the Google key binding.
+ */
 
 define_browser_object_class("google_search_results_links", $label = "Google Search Results",
                             $xpath_expression = "//a[@class='l']");
 
 define_page_mode("google_search_results_mode", "Google Search Results",
                  $enable = function (buffer) {
+                     buffer.local_variables.content_buffer_normal_keymap = google_search_results_keymap;
                      buffer.local_variables.default_browser_object_classes = {
                          __proto__: default_browser_object_classes,
                          follow: "google_search_results_links",
