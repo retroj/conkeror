6c06d18ff5cb4f44b0ed69bfb16e29b65ec87a25
Jeremy Maitin-Shepard <jeremy@jeremyms.com>
2008-04-17 01:06:16 -0400

    Enable within-session XUL caching, since there is no reason not to

--- defaults/preferences/debug.js
+++ defaults/preferences/debug.js
@@ -6,5 +6,9 @@ pref("browser.dom.window.dump.enabled", true);
 
 pref("javascript.options.showInConsole", true);
 pref("javascript.options.strict", true);
-pref("nglayout.debug.disable_xul_cache", true);
+
+// There is no reason not to use the cache within a session, though
+//pref("nglayout.debug.disable_xul_cache", true);
+
+// This prevents cached versions of XUL and JavaScript files from being saved to disk
 pref("nglayout.debug.disable_xul_fastload", true);
