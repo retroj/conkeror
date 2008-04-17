cfa90a4d43d67202750330e23a1c8ae0edb53f48
Ævar Arnfjörð Bjarmason <avar@cpan.org>
2008-04-16 21:44:48 -0400

    youporn.js: Remove unneeded title scraping

--- modules/page-modes/youporn.js
+++ modules/page-modes/youporn.js
@@ -1,24 +1,20 @@
 require("content-buffer.js");
 require("media.js");
 
-const media_youporn_title_regexp = /^.*? - (.*)$/;
+/* We could use the wrappedJSObject to get the player_url but this is
+ * probably more secure, and simpler
+ */
 const media_youporn_flv_regexp = /var player_url = '(.*?)';/;
 
 function media_scrape_youporn(buffer, results) {
     try {
         var doc = buffer.document;
 
-        var title = doc.title;
-        var usable_title_res = media_youporn_title_regexp.exec(title);
-        var usable_title = usable_title_res[0];
-
         let text = doc.documentElement.innerHTML;
         let res = media_youporn_flv_regexp.exec(text);
 
         if (res) {
             results.push(load_spec({uri: res[1],
-                                    suggest_filename_from_uri: false,
-                                    title: usable_title,
                                     filename_extension: "flv",
                                     source_frame: buffer.top_frame,
                                     mime_type: "video/x-flv"}));
