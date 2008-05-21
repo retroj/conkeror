fead87b7fa1156e750dcb01f903e6b8a35e43a37
Nelson Elhage <nelhage@mit.edu>
2008-05-21 17:06:49 -0400

    Add an XKCD page mode

---
+++ modules/page-modes/xkcd.js
@@ -0,0 +1,53 @@
+/**
+ * (C) Copyright 2008 Nelson Elhage
+ *
+ * Use, modification, and distribution are subject to the terms specified in the
+ * COPYING file.
+**/
+
+require("content-buffer.js");
+
+/* Add the XKCD <img> title text below the image in the page */
+function xkcd_add_title(buffer) {
+    var document = buffer.document;
+    // Find the <img> tag
+    var img = document.evaluate(
+        "//div[@id='middleContent']//img",
+        document, null,
+        Ci.nsIDOMXPathResult.ANY_TYPE,null).iterateNext();
+    if(!img) return;
+    var title = img.title;
+    // In some comics, the <img> is a link, so walk up to the surrounding <A>
+    if(img.parentNode.tagName == 'A') {
+        img = img.parentNode;
+    }
+    var node = img.nextSibling;
+    while(node && node.nodeName != 'BR') {
+        node = node.nextSibling;
+    }
+    if(!node) return;
+    // Insert the text inside a <span> with a known ID
+    var text = document.createTextNode(title);
+    var span = document.createElement('span');
+    span.id = 'conkeror:xkcd-title-text';
+    span.appendChild(text);
+    img.parentNode.insertBefore(span, node.nextSibling);
+}
+
+define_page_mode("xkcd_mode","XKCD",
+    $enable = function (buffer) {
+        if(buffer.browser.webProgress.isLoadingDocument) {
+            add_hook.call(buffer, "buffer_dom_content_loaded_hook", xkcd_add_title);
+        } else {
+            xkcd_add_title(buffer);
+        }
+    },
+    // When we disable the mode, remove the <span>
+    $disable = function(buffer) {
+        var span = buffer.document.getElementById('conkeror:xkcd-title-text');
+        if(span) {
+            span.parentNode.removeChild(span);
+        }
+    });
+
+auto_mode_list.push([/^http:\/\/(www\.)?xkcd\..+\/(.*\/)?/, xkcd_mode]);
