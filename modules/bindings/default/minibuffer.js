3b840531771babdc6a39c63941c227548cd69956
John Foerch <jjfoerch@earthlink.net>
2008-09-21 01:45:50 -0400

    C-w binding in minibuffer

--- modules/bindings/default/minibuffer.js
+++ modules/bindings/default/minibuffer.js
@@ -33,6 +33,7 @@ define_key(minibuffer_base_keymap, "C-right", "minibuffer-forward-word");
 define_key(minibuffer_base_keymap, "M-right", "minibuffer-forward-word");
 define_key(minibuffer_base_keymap, "C-y", "minibuffer-cmd_paste");
 define_key(minibuffer_base_keymap, "M-w", "minibuffer-cmd_copy");
+define_key(minibuffer_base_keymap, "C-w", "minibuffer-cmd_cut");
 define_key(minibuffer_base_keymap, "C-k", "minibuffer-cmd_deleteToEndOfLine");
 
 define_key(minibuffer_base_keymap, "home", "minibuffer-beginning-of-line");
