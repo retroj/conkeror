43befe4724c422ee46b84ea24661ab267344f4f5
Jens Petersen <juhpetersen+conkeror@gmail.com>
2008-10-05 23:52:14 +1000

    typo - spelling of automatically

--- modules/download-manager.js
+++ modules/download-manager.js
@@ -1008,7 +1008,7 @@ define_variable("download_temporary_file_open_buffer_delay", 500,
 
 define_variable("download_buffer_automatic_open_target", OPEN_NEW_WINDOW,
                      "Target for download buffers created by the `open_download_buffer_automatically' function.\n" +
-                     "This variable takes effect only if `open_download_buffer_auotmatically' is in " +
+                     "This variable takes effect only if `open_download_buffer_automatically' is in " +
                      "`download_added_hook', as it is by default.");
 
 function open_download_buffer_automatically(info) {
