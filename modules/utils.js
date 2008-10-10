40ddebddfcbcd28a95e523f4659de79ec65f946c
Jeremy Maitin-Shepard <jeremy@jeremyms.com>
2008-10-09 17:57:17 -0700

    define_special_variable: allows defining a getter-setter user-variable

--- modules/utils.js
+++ modules/utils.js
@@ -888,6 +888,17 @@ function define_variable(name, default_value, doc) {
         source_code_reference: get_caller_source_code_reference() });
 }
 
+function define_special_variable(name, getter, setter, doc) {
+    conkeror.__defineGetter__(name, getter);
+    conkeror.__defineSetter__(name, setter);
+    user_variables.put(name,
+                       {
+                           default_value: undefined,
+                           doc: doc,
+                           shortdoc: get_shortdoc_string(doc),
+                           source_code_reference: get_caller_source_code_reference()
+                       });
+}
 
 function register_user_stylesheet(url)
 {
