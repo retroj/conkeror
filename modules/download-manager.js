5355199136d820a0bebacab927dab81a93038110
Nicholas A. Zigarovich <nicktastic@gmail.com>
2008-10-13 13:11:28 -0400

    create_buffer() expects a window as its first argument, not a buffer.

--- modules/download-manager.js
+++ modules/download-manager.js
@@ -1018,7 +1018,7 @@ function open_download_buffer_automatically(info) {
         target = OPEN_NEW_WINDOW;
     if (info.temporary_status == DOWNLOAD_NOT_TEMPORARY ||
         !(download_temporary_file_open_buffer_delay > 0))
-        create_buffer(buf, buffer_creator(download_buffer, $info = info), target);
+        create_buffer(buf.window, buffer_creator(download_buffer, $info = info), target);
     else {
         var timer = null;
         function finish() {
@@ -1027,7 +1027,7 @@ function open_download_buffer_automatically(info) {
         add_hook.call(info, "download_finished_hook", finish);
         timer = call_after_timeout(function () {
                 remove_hook.call(info, "download_finished_hook", finish);
-                create_buffer(buf, buffer_creator(download_buffer, $info = info), target);
+                create_buffer(buf.window, buffer_creator(download_buffer, $info = info), target);
             }, download_temporary_file_open_buffer_delay);
     }
 }
