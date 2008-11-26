805dfa6868cfd59bfd31ae8d69dc528920f8583a
skorpan <deniz@stalin>
2008-11-27 00:56:40 +0100

    content-buffer/normal.js: C-x C-s -> save-page

--- modules/bindings/default/content-buffer/normal.js
+++ modules/bindings/default/content-buffer/normal.js
@@ -23,6 +23,7 @@ define_key(content_buffer_normal_keymap, "l", "go-back");
 define_key(content_buffer_normal_keymap, "r", "reload");
 define_key(content_buffer_normal_keymap, "g", "find-url");
 define_key(content_buffer_normal_keymap, "C-x C-v", "find-alternate-url");
+define_key(content_buffer_normal_keymap, "C-x C-s", "save-page");
 
 
 define_key(content_buffer_normal_keymap, "C-g", "stop-loading");
