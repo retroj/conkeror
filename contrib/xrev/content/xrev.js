672bcf45001832a7d41911920b8a84b5db214e74
John Foerch <jjfoerch@earthlink.net>
2008-12-17 21:34:11 -0500

    xrev: show event info in the gui

--- contrib/xrev/content/xrev.js
+++ contrib/xrev/content/xrev.js
@@ -5,9 +5,11 @@
  * COPYING file.
 **/
 
+var XULNS = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul";
+
 // key_event_props: what properties of keyboard events do we care about?
 var key_event_props = [
-    'timeStamp', 'type', 'charCode', 'keyCode', 'altKey',
+    'type', 'charCode', 'keyCode', 'altKey',
     'ctrlKey', 'shiftKey', 'metaKey', 'eventPhase',
     'isChar', 'which'];
 
@@ -16,12 +18,6 @@ function dumpln (line) {
     dump(line + "\n");
 }
 
-function key_event_handler (event) {
-    dumpln([event[k].toString().pad(' ',11)
-            for each (k in key_event_props)].join(''));
-}
-
-
 function str_pad (chr, len) {
     var s = this.toString();
     while (len - s.length > 0) {
@@ -29,8 +25,42 @@ function str_pad (chr, len) {
     }
     return s;
 }
-
 String.prototype.pad = str_pad;
 
-dumpln([k.pad(' ',11) for each (k in key_event_props)].join(''));
+
+
+function key_event_handler (event) {
+    dumpln([event[k].toString().pad(' ',11)
+            for each (k in key_event_props)].join(''));
+    var table = document.getElementById('event-table');
+    var row = document.createElementNS(XULNS,"listitem");
+    var cap;
+    var k;
+    for each (k in key_event_props) {
+        cap = document.createElementNS(XULNS,"listcell");
+        cap.setAttribute("label", event[k]);
+        row.appendChild(cap);
+    }
+    table.appendChild(row);
+    table.ensureElementIsVisible(row);
+}
+
+
+function onload_handler () {
+    dumpln([k.pad(' ',11) for each (k in key_event_props)].join(''));
+    var table = document.getElementById('event-table');
+    var head = document.createElementNS(XULNS,"listhead");
+    var coldef = document.createElementNS(XULNS,"listcols");
+    var t, u;
+    for each (k in key_event_props) {
+        t = document.createElementNS(XULNS,"listheader");
+        t.setAttribute("label", k);
+        u = document.createElementNS(XULNS,"listcol");
+        u.setAttribute("flex", "1");
+        head.appendChild(t);
+        coldef.appendChild(u);
+    }
+    table.appendChild(head);
+    table.appendChild(coldef);
+}
 
