retroj/conkeror

commit 358ec6e4553142830469c07aef1dbd931384111c

Let search page webjumps with no arguments fall back to a home page.
Most webjumps are used as a shorthand to a web site search page.  But
when no arguments are supplied to the webjump, the effect is either an
error from conkeror (when there are no spaces) or a search with a
blank search term.  Neither is particularly useful.

This commit alters the default webjump handler so that a webjump with
no arguments will visit a relevant page, usually the associated home
page.  In most cases this is just the base URL of the webjump.  For
instance, visiting the emacswiki webjump with no arguments will now
load the emacswiki.org front page.

Most webjumps gain this new functionality without needing
modification.  However in some cases the search url is unrelated to
the desired home page url.  In this case the webjump definition can
supply an array of two elements; the first is the search url and the
second is the home page url.  The provided clhs and xulplanet webjumps
are treated in this way.

A similar fall back facility could be provided for search engine
webjumps, but that is not done here.

The meaning of the webjump no_argument keyword is extended to support
these optional arguments.  As previously, a value of true means that
no arguments are allowed and a false value means that arguments are
required.  The extension is that any other value ("maybe" is used)
means that arguments are optional.

This commit also includes a small fix to match_webjump().  A webjump
keyword followed by spaces is now interpreted as meaning no arguments.

The function compute_url_pre_path() is also included to calculate a
base URL from a given URL.

modules/webjump.js
@@ -17,20 +17,31 @@ function define_webjump(key, handler) {
 
     var no_argument = arguments.$no_argument;
 
-    if (typeof(handler) == "string") {
-        let template = handler;
+    make_handler = function(template, alternative) {
         let b = template.indexOf('%s');
         if (b == -1)
             no_argument = true;
-        handler = function (arg) {
+        if (alternative == null)
+            alternative = compute_url_pre_path(template);
+        if (alternative && !no_argument)
+            no_argument = "maybe";
+        return function (arg) {
             var a = b + 2;
+            if (arg == null)
+                return alternative;
             // Just return the same string if it doesn't contain a %s
             if (b == -1)
                 return template;
-            else
-                return template.substr(0,b) + encodeURIComponent(arg) + template.substring(a);
+            return template.substr(0,b) + encodeURIComponent(arg) + template.substring(a);
         };
     }
+
+    if (typeof(handler) == "string")
+        handler = make_handler(handler);
+    if (typeof(handler) == "object")
+        // An array of a template and an alternative url (for no args)
+        handler = make_handler(handler[0], handler[1]);
+
     webjumps.put(key,
                  {key: key,
                   handler: handler, completer: arguments.$completer,
@@ -65,14 +76,15 @@ function define_default_webjumps()
     add_webjump("clusty",     "http://www.clusty.com/search?query=%s");
     add_webjump("slang",      "http://www.urbandictionary.com/define.php?term=%s");
     add_webjump("dictionary", "http://dictionary.reference.com/search?q=%s");
-    add_webjump("xulplanet",  "http://www.google.com/custom?q=%s&cof=S%3A"+
+    add_webjump("xulplanet",  ["http://www.google.com/custom?q=%s&cof=S%3A"+
                 "http%3A%2F%2Fwww.xulplanet.com%3BAH%3Aleft%3BLH%3A65%3BLC"+
                 "%3A4682B4%3BL%3Ahttp%3A%2F%2Fwww.xulplanet.com%2Fimages%2F"+
                 "xulplanet.png%3BALC%3Ablue%3BLW%3A215%3BAWFID%3A0979f384d5"+
-                "181409%3B&domains=xulplanet.com&sitesearch=xulplanet.com&sa=Go");
+                "181409%3B&domains=xulplanet.com&sitesearch=xulplanet.com&sa=Go",
+                "http://xulplanet.com"]);
     add_webjump("image",      "http://images.google.com/images?q=%s");
     add_webjump("imdb",       "http://www.imdb.com/find?s=all&q=%s&x=0&y=0");
-    add_webjump("clhs",       "http://www.xach.com/clhs?q=%s");
+    add_webjump("clhs",       ["http://www.xach.com/clhs?q=%s", "http://www.lispworks.com/documentation/HyperSpec/Front/index.htm"]);
     add_webjump("emacswiki",  "http://www.emacswiki.org/cgi-bin/wiki?search=%s");
     add_webjump("cliki",      "http://www.cliki.net/admin/search?words=%s");
     add_webjump("ratpoisonwiki", "http://ratpoison.antidesktop.net/?search=%s");
@@ -96,6 +108,8 @@ function match_webjump(str) {
     } else {
         key = str.substring(0, sp);
         arg = str.substring(sp + 1);
+        if (/^\s*$/.test(arg))
+            arg = null;
     }
 
     // Look for an exact match
@@ -150,7 +164,7 @@ function webjump_completer()
 {
     let base_completer = prefix_completer(
         $completions = [ v for ([k,v] in webjumps.iterator()) ],
-        $get_string = function (x) x.key + (x.no_argument ? "" : " "),
+        $get_string = function (x) x.key + (x.no_argument==true ? "" : " "),
         $get_description = function (x) x.description || "");
 
     return function(input, pos, conservative) {

modules/utils.js
@@ -1266,6 +1266,15 @@ function compute_url_up_path (url)
 }
 
 
+function compute_url_pre_path (url)
+{
+    var new_url = Cc["@mozilla.org/network/standard-url;1"]
+        .createInstance (Ci.nsIURL);
+    new_url.spec = url;
+    return new_url.prePath;
+}
+
+
 /* remove_duplicates_filter returns a function that can be
  * used in Array.filter.  It removes duplicates.
  */

commit 02ac53b4fe7b5d12c23538aca537571676908dc6

fix delicious and lastfm webjumps
These got broken in 358ec6e4553142830469c07aef1dbd931384111c, when
the default behavior was changed for webjump calls with no arguments.
This fix is a hack.  The whole thing of webjumps without args needs
to be rethought.

modules/webjump.js
@@ -17,7 +17,7 @@ function define_webjump(key, handler) {
 
     var no_argument = arguments.$no_argument;
 
-    make_handler = function(template, alternative) {
+    function make_handler (template, alternative) {
         let b = template.indexOf('%s');
         if (b == -1)
             no_argument = true;
@@ -54,15 +54,20 @@ var add_webjump = define_webjump;
 
 function add_delicious_webjumps (username)
 {
-    define_webjump("delicious", "http://del.icio.us/" + username);
-    define_webjump("adelicious", "javascript:location.href='http://del.icio.us/" + username + "?v=2&url='+encodeURIComponent(location.href)+'&title='+encodeURIComponent(document.title);");
-    define_webjump("sdelicious", "http://delicious.com/search?p=%s&u=" + username + "&chk=&context=userposts&fr=del_icio_us&lc=1");
+    var delicious_url = "http://del.icio.us/" + username;
+    var adelicious_url = "javascript:location.href='http://del.icio.us/"+username+
+        "?v=2&url='+encodeURIComponent(location.href)+'&title='+encodeURIComponent(document.title);";
+    define_webjump("delicious", [delicious_url, delicious_url]);
+    define_webjump("adelicious", [adelicious_url, adelicious_url]);
+    define_webjump("sdelicious", "http://delicious.com/search?p=%s&u="+username+
+                   "&chk=&context=userposts&fr=del_icio_us&lc=1");
     define_webjump("sadelicious", "http://del.icio.us/search/all?search=%s");
 }
 
 function add_lastfm_webjumps(username)
 {
-    define_webjump("lastfm", "http://www.last.fm/user/" + (username ? username : ""));
+    var lastfm_url = "http://www.last.fm/user/" + (username ? username : "");
+    define_webjump("lastfm", [lastfm_url, lastfm_url]);
     define_webjump("lastfm-user", "http://www.last.fm/user/%s");
     define_webjump("lastfm-music", "http://www.last.fm/search?m=all&q=%s");
     define_webjump("lastfm-group", "http://www.last.fm/users/groups?s_bio=%s");
@@ -175,8 +180,8 @@ function webjump_completer()
 {
     let base_completer = prefix_completer(
         $completions = [ v for ([k,v] in webjumps.iterator()) ],
-        $get_string = function (x) x.key + (x.no_argument==true ? "" : " "),
-        $get_description = function (x) x.description || "");
+        $get_string = function (x) { return x.key + (x.no_argument==true ? "" : " "); },
+        $get_description = function (x) { return x.description || ""; });
 
     return function(input, pos, conservative) {
         let str = input.substring(0,pos);