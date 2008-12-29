d09d3a35aab12d20970dbf779273fb8b023a3036
John Foerch <jjfoerch@earthlink.net>
2008-12-28 20:08:39 -0500

    walnut: assert_null function

--- modules/walnut.js
+++ modules/walnut.js
@@ -33,6 +33,13 @@ function assert_error (fn, name) {
     return true;
 }
 
+function assert_null (got, name) {
+    if (name == null) name = "unnamed";
+    if (got !== null)
+        throw new Error(name+" failed. got <"+got+">.");
+    return true;
+}
+
 // sanity checks
 { let failed = false;
   try { assert_equals(1,0); } catch (e) { failed = true; }
@@ -53,5 +60,8 @@ function assert_error (fn, name) {
   failed = false;
   try { assert_error(function () {}); } catch (e) { failed = true; }
   assert(failed, "assert_error: sanity check 2");
+
+  assert_null(null, "assert_null: sanity check 1");
+  assert_error(function () { assert_null(false); }, "assert_null: sanity check 2");
 }
 
