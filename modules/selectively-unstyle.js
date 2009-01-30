03ded982cc8ab320e0056cf87b49bc2b5210f760
John Foerch <jjfoerch@earthlink.net>
2009-01-30 13:12:20 -0500

    selectively-unstyle: new module
    Example of how to use it:
    
      require('selectively-unstyle.js');
      let (google = build_url_regex(
               $domain = "google",
               $allow_www = true,
               $path = "search?",
               $tlds = ["com", "co.uk", "de", "dk", "es",
                        "fr", "it", "no", "se", "uk"])) {
          selectively_unstyle_alist.push([google, function () { return true; }]);
      };

---
+++ modules/selectively-unstyle.js
@@ -0,0 +1,31 @@
+/**
+ * (C) Copyright 2009 John J. Foerch
+ *
+ * Use, modification, and distribution are subject to the terms specified in the
+ * COPYING file.
+**/
+
+define_variable('selectively_unstyle_alist', [],
+    "Alist mapping url-matching regular expressions to predicates to "+
+    "selectively disable stylesheets.  All predicates corresponding to "+
+    "matching regexps will be used.  Predicates are functions of one "+
+    "argument.  They receive a styleSheet object as their argument, and "+
+    "if they return true, that stylesheet will be disabled.");
+
+
+function selectively_unstyle (buffer) {
+    var uri = buffer.current_URI.spec;
+    for each (let entry in selectively_unstyle_alist) {
+        if (entry[0](uri)) {
+            let pred = entry[1];
+            for each (var sheet in buffer.document.styleSheets) {
+                if (pred(sheet)) {
+                    sheet.disabled = true;
+                }
+            }
+        }
+    }
+}
+
+add_hook("buffer_loaded_hook", selectively_unstyle);
+
