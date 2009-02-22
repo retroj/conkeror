retroj/conkeror

commit 080e20e63088c1f3f39f5a3ea22c17147fe810c2

download manager: show display name, allow target file name to change
The download manager will now update the display of the target file
name each time there is a change in download state.  If the display
name differs from the (basename of) the target name, it is shown
parenthetically.  Changes to the names in hooks are thus reflected in
the display.

The download manager handling of the target file is changed.  It is
now maintained explicitly and can be specified in register_download.
This is necessary because the targetFile property is immutable.

These changes allow, for instance, downloads to a file with a
temporary name that is changed to the final name when the download is
finished.

modules/download-manager.js
@@ -169,17 +169,24 @@ const DOWNLOAD_NOT_TEMPORARY = 0;
 const DOWNLOAD_TEMPORARY_FOR_ACTION = 1;
 const DOWNLOAD_TEMPORARY_FOR_COMMAND = 2;
 
-function download_info(source_buffer, mozilla_info) {
+function download_info(source_buffer, mozilla_info, target_file) {
     this.source_buffer = source_buffer;
+    this.target_file = target_file;
     if (mozilla_info != null)
         this.attach(mozilla_info);
 }
 download_info.prototype = {
     attach : function (mozilla_info) {
+        if (!this.target_file)
+            target_file = this.mozilla_info.targetFile;
+        else if (this.target_file.path != mozilla_info.targetFile.path)
+            throw interactive_error("Download target file unexpected.");
         this.mozilla_info = mozilla_info;
         id_to_download_info[mozilla_info.id] = this;
         download_added_hook.run(this);
     },
+
+    target_file : null,
 
     shell_command : null,
 
@@ -202,7 +209,7 @@ download_info.prototype = {
 
     // Reflectors to properties of nsIDownload
     get state () { return this.mozilla_info.state; },
-    get target_file () { return this.mozilla_info.targetFile; },
+    get display_name () { return this.mozilla_info.displayName; },
     get amount_transferred () { return this.mozilla_info.amountTransferred; },
     get percent_complete () { return this.mozilla_info.percentComplete; },
     get size () {
@@ -226,6 +233,14 @@ download_info.prototype = {
     },
     get id () { return this.mozilla_info.id; },
     get referrer () { return this.mozilla_info.referrer; },
+
+    target_file_text : function() {
+        let target = this.target_file.path;
+        let display = this.display_name;
+        if (target.indexOf(display, target.length - display.length) == -1)
+            target += " (" + display + ")";
+        return target;
+    },
 
     throw_if_removed : function () {
         if (this.removed)
@@ -357,8 +372,8 @@ download_info.prototype = {
 var define_download_local_hook = simple_local_hook_definer();
 
 // FIXME: add more parameters
-function register_download(buffer, source_uri) {
-    var info = new download_info(buffer);
+function register_download(buffer, source_uri, target_file) {
+    var info = new download_info(buffer, null, target_file);
     info.registered_time_stamp = Date.now();
     info.registered_source_uri = source_uri;
     unmanaged_download_info_list.push(info);
@@ -599,6 +614,7 @@ download_buffer.prototype = {
 
         // Remove all node references
         delete this.status_textnode;
+        delete this.target_file_node;
         delete this.transferred_div_node;
         delete this.transferred_textnode;
         delete this.progress_container_node;
@@ -719,7 +735,7 @@ download_buffer.prototype = {
             target_label = "Target:";
         g.text(target_label, label);
         value = g.element("div", div, "class", "download-value");
-        g.text(info.target_file.path, value);
+        this.target_file_node = g.text("", value);
 
         div = g.element("div", d.body, "class", "download-info", "id", "download-mime-type");
         label = g.element("div", div, "class", "download-label");
@@ -791,6 +807,7 @@ download_buffer.prototype = {
             break;
         }
         this.status_textnode.nodeValue = label + ":";
+        this.target_file_node.nodeValue = info.target_file_text();
         this.update_time_field();
 
         var tran_text = "";

commit ecb3492e1561ee9ffca959eaa684e58c390dc03c

download manager: display downloads properly again
Commit 080e20e (download manager: show display name, allow target file
name to change, 2009-02-18) broke the download manager display of most
downloads.

There are two separate interfaces for downloading files that use
different methods; (1) the save interactive command and (2) choosing
"save" in response to the "Action to perform" prompt when visiting a
download url.  That commit was tested for case (1) but not case (2).

The one line of code changed in this commit had two errors;
this.mozilla_info was not yet set (it is set a couple of lines later)
and mozilla_info.targetFile is not set at the time that attach() is
called.

The fix restores the previous behaviour of target_file being a getter,
if it is not already set.

It may be that the two methods could be changed to use the same
underlying interface and so simplify this code and obviate the need
for the changes in commit 9645e93 (save_uri: optionally use a
temporary download file, 2009-02-18).

modules/download-manager.js
@@ -178,7 +178,9 @@ function download_info(source_buffer, mozilla_info, target_file) {
 download_info.prototype = {
     attach : function (mozilla_info) {
         if (!this.target_file)
-            target_file = this.mozilla_info.targetFile;
+            this.__defineGetter__("target_file", function(){
+                    return this.mozilla_info.targetFile;
+                });
         else if (this.target_file.path != mozilla_info.targetFile.path)
             throw interactive_error("Download target file unexpected.");
         this.mozilla_info = mozilla_info;