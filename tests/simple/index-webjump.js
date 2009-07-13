1dc4a6e2964e4045c7f6cc9fcde84503f1b9466f
David Kettler <kettler@internode.on.net>
2009-07-13 15:46:28 +0930

    index-webjump: test suite

---
+++ tests/simple/index-webjump.js
@@ -0,0 +1,52 @@
+require('walnut.js');
+require('index-webjump.js');
+
+{ let suite = {
+      suite_setup: function () {
+          this.real_webjumps = webjumps;
+          this.real_index_webjumps = index_webjumps;
+          conkeror.webjumps = {};
+          conkeror.index_webjumps = {};
+      },
+      suite_teardown: function () {
+          conkeror.webjumps = this.real_webjumps;
+          conkeror.index_webjumps = this.real_index_webjumps;
+      },
+      path: conkeror_source_code_path + "/tests/simple",
+      test_xpath_webjump: function () {
+          define_xpath_webjump(
+              "xpath", "http://dummy/xpath", '//xhtml:a[@class="index"]',
+              $index_file = this.path + '/xpath-webjump-test.xhtml');
+          assert_equals(get_webjump("xpath foo"), "foo");
+          var w = index_webjumps.xpath;
+          w.extract_completions();
+          assert_equals(w.completions.length, 2);
+          assert_equals(w.completions[0][1], "The bar");
+          assert_equals(w.completions[0][0], "http://dummy/xpath/bar");
+          assert_equals(w.completions[1][1], "The foo");
+          assert_equals(w.completions[1][0], "http://dummy/foo");
+      },
+      test_gitweb_webjump: function() {
+          define_gitweb_summary_webjump(
+              "gitweb", "http://dummy/gitweb", $default = "bar",
+              $opml_file = this.path + '/gitweb-webjump-test.opml');
+          assert_equals(get_webjump("gitweb"),
+                        "http://dummy/gitweb/gitweb.cgi?p=bar.git;a=summary");
+          assert_equals(get_webjump("gitweb foo"),
+                        "http://dummy/gitweb/gitweb.cgi?p=foo.git;a=summary");
+          var w = index_webjumps.gitweb;
+          w.extract_completions();
+          assert_equals(w.completions.length, 2);
+          assert_equals(w.completions[0][0], "bar");
+          assert_equals(w.completions[1][0], "foo");
+      },
+  };
+  if (conkeror_source_code_path)
+      walnut_run(suite);
+  else
+      dumpln('Suite setup failed; conkeror_source_code_path not set.' +
+             '  Try:\n' +
+             '  conkeror -q -batch' +
+             ' -e "conkeror_source_code_path=\\"$PWD\\";"' +
+             ' -l tests/simple/index-webjump.js');
+}
