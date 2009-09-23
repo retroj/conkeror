9440cfa6fd76d67eae524a3518250b9de274793b
John Foerch <jjfoerch@earthlink.net>
2009-09-22 23:52:44 -0400

    whitespace

--- modules/favicon.js
+++ modules/favicon.js
@@ -8,22 +8,36 @@
  * COPYING file.
 **/
 
-const favicon_service = Cc["@mozilla.org/browser/favicon-service;1"].getService(Ci.nsIFaviconService);
+const favicon_service = Cc["@mozilla.org/browser/favicon-service;1"]
+    .getService(Ci.nsIFaviconService);
+
+
+define_variable("favicon_image_max_size", 1024,
+    "Maximum (pixel) width and height of an image document that "+
+    "is considered for use as a favicon.");
 
 define_buffer_local_hook("buffer_favicon_change_hook");
 
-function favicon_content_buffer_started_loading(buffer) {
+
+function favicon_set (buffer, icon_url) {
+    buffer.favicon = icon_url.spec;
+    favicon_service.setAndLoadFaviconForPage(buffer.current_uri,
+                                             icon_url, false);
+    buffer_favicon_change_hook.run(buffer);
+}
+
+
+function favicon_content_buffer_started_loading (buffer) {
     var old = buffer.favicon;
     buffer.favicon = null;
     if (old != null)
         buffer_favicon_change_hook.run(buffer);
 }
-add_hook("content_buffer_started_loading_hook", favicon_content_buffer_started_loading);
+add_hook("content_buffer_started_loading_hook",
+         favicon_content_buffer_started_loading);
 
-define_variable("favicon_image_max_size", 1024, "Maximum (pixel) width and height of an image document that is considered for use as a favicon.");
-var favicon_image_max_size = 1024;
 
-function favicon_content_buffer_finished_loading(buffer) {
+function favicon_content_buffer_finished_loading (buffer) {
     if (buffer.favicon != null)
         return;
 
@@ -31,7 +45,8 @@ function favicon_content_buffer_finished_loading(buffer) {
         var req = buffer.document.imageRequest;
         if (req && req.image &&
             req.image.width <= favicon_image_max_size  &&
-            req.image.height <= favicon_image_max_size) {
+            req.image.height <= favicon_image_max_size)
+        {
             favicon_set(buffer, buffer.current_uri);
             return;
         }
@@ -46,12 +61,12 @@ function favicon_content_buffer_finished_loading(buffer) {
     if (!favicon_service.isFailedFavicon(icon_url))
         favicon_set(buffer, icon_url);
 }
-add_hook("content_buffer_finished_loading_hook", favicon_content_buffer_finished_loading);
+add_hook("content_buffer_finished_loading_hook",
+         favicon_content_buffer_finished_loading);
 
-var content_policy_service = Cc["@mozilla.org/layout/content-policy;1"].getService(Ci.nsIContentPolicy);
 
-function favicon_content_buffer_dom_link_added(buffer, event) {
-    var link = event.originalTarget;
+function favicon_content_buffer_dom_link_added (buffer, event) {
+   var link = event.originalTarget;
 
     if (!link || !link.ownerDocument || !link.rel || !link.href)
         return;
@@ -63,7 +78,8 @@ function favicon_content_buffer_dom_link_added(buffer, event) {
 
     /* FIXME: perhaps worry about newURI throwing an exception */
     var target_doc = link.ownerDocument;
-    var ios = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
+    var ios = Cc["@mozilla.org/network/io-service;1"]
+        .getService(Ci.nsIIOService);
     var uri = ios.newURI(link.href, target_doc.characterSet, null);
 
     if (favicon_service.isFailedFavicon(uri))
@@ -73,11 +89,15 @@ function favicon_content_buffer_dom_link_added(buffer, event) {
     // error pages can load their favicon, to be on the safe side,
     // only allow chrome:// favicons
     const about_neterr = "about:neterror?";
-    if (target_doc.documentURI.substr(0, about_neterr.length) != about_neterr || !uri.schemeIs("chrome")) {
-        var ssm = Cc["@mozilla.org/scriptsecuritymanager;1"].getService(Ci.nsIScriptSecurityManager);
+    if (target_doc.documentURI.substr(0, about_neterr.length) != about_neterr
+        || !uri.schemeIs("chrome"))
+    {
+        var ssm = Cc["@mozilla.org/scriptsecuritymanager;1"]
+            .getService(Ci.nsIScriptSecurityManager);
         try {
-            ssm.checkLoadURIWithPrincipal(target_doc.nodePrincipal, uri,
-                                          Ci.nsIScriptSecurityManager.DISALLOW_SCRIPT);
+            ssm.checkLoadURIWithPrincipal(
+                target_doc.nodePrincipal, uri,
+                Ci.nsIScriptSecurityManager.DISALLOW_SCRIPT);
         } catch(e) {
             return;
         }
@@ -89,19 +109,17 @@ function favicon_content_buffer_dom_link_added(buffer, event) {
     }
 
     // Security says okay, now ask content policy
+    var content_policy_service = Cc["@mozilla.org/layout/content-policy;1"]
+        .getService(Ci.nsIContentPolicy);
     if (content_policy_service.shouldLoad(Ci.nsIContentPolicy.TYPE_IMAGE,
                                           uri, target_doc.documentURIObject,
                                           link, link.type, null)
         != Ci.nsIContentPolicy.ACCEPT)
+    {
         return;
+    }
 
     favicon_set(buffer, uri);
 }
-add_hook("content_buffer_dom_link_added_hook", favicon_content_buffer_dom_link_added);
-
-function favicon_set(buffer, icon_url) {
-    buffer.favicon = icon_url.spec;
-
-    favicon_service.setAndLoadFaviconForPage(buffer.current_uri, icon_url, false);
-    buffer_favicon_change_hook.run(buffer);
-}
+add_hook("content_buffer_dom_link_added_hook",
+         favicon_content_buffer_dom_link_added);
