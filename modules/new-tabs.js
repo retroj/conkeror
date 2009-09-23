retroj/conkeror

commit 1e1a96da42d12b107dc7c6c0041a355f9763c7a1

move input-mode framework into buffer.js, eliminating circular dependency
This commit fixes http://bugs.conkeror.org/issue160 which was caused by
a frameset document in which a form existed in one frame, whose target
was another frame.  When the form was submitted, with focus still on the
input-box, results would be loaded in this other frame.  That load would
trigger a location-change, which would in turn trigger conkeror
erroneously switching to normal-input-mode.  The fix was to only use the
function content_buffer_update_input_mode_for_focus to determine the
proper mode, and never calling normal_input_mode directly.

added create_buffer_early_hook, fixes http://bugs.conkeror.org/issue102

under certain conditions, it is possible for a document title to be
changed before the buffer constructor has run to completion.  This causes
tab_bar_update_buffer_title to get run before tab_bar_add_buffer has been
run, so buffer.tab does not yet exist.

This commit begins to establish the idea of modular input-mode systems.
There are further changes that need to be made in order to complete that
project, and many of them have been marked with explanatory XXX and
FIXME notes.

modules/new-tabs.js
@@ -35,7 +35,7 @@ function tab_bar(window) {
     after.parentNode.insertBefore(scrollbox, after);
 
     add_hook.call(window, "select_buffer_hook", tab_bar_select_buffer);
-    add_hook.call(window, "create_buffer_hook", tab_bar_add_buffer);
+    add_hook.call(window, "create_buffer_early_hook", tab_bar_add_buffer);
     add_hook.call(window, "kill_buffer_hook", tab_bar_kill_buffer);
     add_hook.call(window, "buffer_title_change_hook", tab_bar_update_buffer_title);
     add_hook.call(window, "buffer_description_change_hook", tab_bar_update_buffer_title);
@@ -52,7 +52,7 @@ function tab_bar(window) {
  */
 tab_bar.prototype.destroy = function () {
     remove_hook.call(this.window, "select_buffer_hook", tab_bar_select_buffer);
-    remove_hook.call(this.window, "create_buffer_hook", tab_bar_add_buffer);
+    remove_hook.call(this.window, "create_buffer_early_hook", tab_bar_add_buffer);
     remove_hook.call(this.window, "kill_buffer_hook", tab_bar_kill_buffer);
     remove_hook.call(this.window, "buffer_title_change_hook", tab_bar_update_buffer_title);
     remove_hook.call(this.window, "buffer_description_change_hook", tab_bar_update_buffer_title);

modules/tab-bar.js
@@ -18,7 +18,7 @@ function tab_bar(window) {
     after.parentNode.insertBefore(element, after);
 
     add_hook.call(window, "select_buffer_hook", tab_bar_select_buffer);
-    add_hook.call(window, "create_buffer_hook", tab_bar_add_buffer);
+    add_hook.call(window, "create_buffer_early_hook", tab_bar_add_buffer);
     add_hook.call(window, "kill_buffer_hook", tab_bar_kill_buffer);
     add_hook.call(window, "buffer_title_change_hook", tab_bar_update_buffer_title);
     add_hook.call(window, "buffer_description_change_hook", tab_bar_update_buffer_title);
@@ -30,7 +30,7 @@ function tab_bar(window) {
 }
 tab_bar.prototype.destroy = function () {
     remove_hook.call(this.window, "select_buffer_hook", tab_bar_select_buffer);
-    remove_hook.call(this.window, "create_buffer_hook", tab_bar_add_buffer);
+    remove_hook.call(this.window, "create_buffer_early_hook", tab_bar_add_buffer);
     remove_hook.call(this.window, "kill_buffer_hook", tab_bar_kill_buffer);
     remove_hook.call(this.window, "buffer_title_change_hook", tab_bar_update_buffer_title);
     remove_hook.call(this.window, "buffer_description_change_hook", tab_bar_update_buffer_title);

modules/buffer.js
@@ -22,6 +22,7 @@ function define_current_buffer_hook (hook_name, existing_hook) {
 define_buffer_local_hook("buffer_title_change_hook");
 define_buffer_local_hook("buffer_description_change_hook");
 define_buffer_local_hook("select_buffer_hook");
+define_buffer_local_hook("create_buffer_early_hook");
 define_buffer_local_hook("create_buffer_hook");
 define_buffer_local_hook("kill_buffer_hook");
 define_buffer_local_hook("buffer_scroll_hook");
@@ -124,6 +125,10 @@ function buffer (window, element) {
             if (allow_browser_window_close)
                 kill_buffer(buffer, true);
         }, true);
+
+    // When create_buffer_hook_early runs, basic buffer properties
+    // will be available, but not the properties subclasses.
+    create_buffer_early_hook.run(this);
 
     this.constructor_end();
 }
@@ -691,10 +696,12 @@ function for_each_buffer (f) {
     for_each_window(function (w) { w.buffers.for_each(f); });
 }
 
-require_later("content-buffer.js");
+
+/*
+ * BUFFER MODES
+ */
 
 var mode_functions = {};
-
 var mode_display_names = {};
 
 define_buffer_local_hook("buffer_mode_change_hook");
@@ -818,3 +825,57 @@ minibuffer_mode_indicator.prototype = {
 };
 define_global_window_mode("minibuffer_mode_indicator", "window_initialize_hook");
 minibuffer_mode_indicator_mode(true);
+
+
+
+/*
+ * INPUT MODES
+ */
+define_current_buffer_hook("current_buffer_input_mode_change_hook", "input_mode_change_hook");
+
+let (input_mode_keymaps = {}) {
+    function define_input_mode (base_name, display_name, keymap_name, doc) {
+        var name = base_name + "_input_mode";
+        input_mode_keymaps[name] = keymap_name;
+        define_buffer_mode(name,
+                           display_name,
+                           $class = "input_mode",
+                           $enable = function (buffer) {
+                               check_buffer(buffer, content_buffer);
+                               buffer_update_keymap_for_input_mode(buffer);
+                           },
+                           $disable = false,
+                           $doc = doc);
+    }
+    ignore_function_for_get_caller_source_code_reference("define_input_mode");
+
+    function buffer_update_keymap_for_input_mode (buffer) {
+        if (buffer.input_mode)
+            buffer.keymap = conkeror[input_mode_keymaps[buffer.input_mode]];
+    }
+}
+
+
+function minibuffer_input_mode_indicator (window) {
+    this.window = window;
+    this.hook_func = method_caller(this, this.update);
+    add_hook.call(window, "select_buffer_hook", this.hook_func);
+    add_hook.call(window, "current_buffer_input_mode_change_hook", this.hook_func);
+    this.update();
+}
+
+minibuffer_input_mode_indicator.prototype = {
+    update : function () {
+        var buf = this.window.buffers.current;
+        var mode = buf.input_mode;
+        if (mode)
+            this.window.minibuffer.element.className = "minibuffer-" + buf.input_mode.replace("_","-","g");
+    },
+    uninstall : function () {
+        remove_hook.call(window, "select_buffer_hook", this.hook_func);
+        remove_hook.call(window, "current_buffer_input_mode_change_hook", this.hook_func);
+    }
+};
+
+define_global_window_mode("minibuffer_input_mode_indicator", "window_initialize_hook");
+minibuffer_input_mode_indicator_mode(true);

modules/content-buffer-input.js
@@ -1,41 +1,25 @@
 /**
  * (C) Copyright 2007-2008 Jeremy Maitin-Shepard
- * (C) Copyright 2008 John J. Foerch
+ * (C) Copyright 2008-2009 John J. Foerch
  *
  * Use, modification, and distribution are subject to the terms specified in the
  * COPYING file.
 **/
 
+/**
+ * This file contains the default input-mode system.  An input-mode
+ * system is a set of input-modes along with code that coordinates
+ * switching among them based on user actions and focus-context.
+ */
+
 require("content-buffer.js");
 
-define_current_buffer_hook("current_buffer_input_mode_change_hook", "input_mode_change_hook");
 
-var content_buffer_input_mode_keymaps = {};
+/*
+ * DEFAULT INPUT-MODE SYSTEM
+ */
 
-function define_input_mode(base_name, display_name, keymap_name, doc) {
-    var name = base_name + "_input_mode";
-    content_buffer_input_mode_keymaps[name] = keymap_name;
-    define_buffer_mode(name,
-                       display_name,
-                       $class = "input_mode",
-                       $enable = function (buffer) {
-                           check_buffer(buffer, content_buffer);
-                           content_buffer_update_keymap_for_input_mode(buffer);
-                       },
-                       $disable = false,
-                       $doc = doc);
-}
-ignore_function_for_get_caller_source_code_reference("define_input_mode");
-
-function content_buffer_update_keymap_for_input_mode (buffer) {
-    if (buffer.input_mode)
-        buffer.keymap = conkeror[content_buffer_input_mode_keymaps[buffer.input_mode]];
-}
-
-add_hook("content_buffer_location_change_hook", function (buf) { normal_input_mode(buf, true); });
-
-
-// Input mode for "normal" view mode
+// Input mode for "normal" view mode.  You might also call it "scroll" mode.
 define_input_mode("normal", null, "content_buffer_normal_keymap");
 
 // Input modes for form elements
@@ -45,23 +29,16 @@ define_input_mode("textarea", "input:TEXTAREA", "content_buffer_textarea_keymap"
 define_input_mode("richedit", "input:RICHEDIT", "content_buffer_richedit_keymap");
 define_input_mode("checkbox", "input:CHECKBOX/RADIOBUTTON", "content_buffer_checkbox_keymap");
 
-// Input modes for sending key events to gecko
-define_input_mode(
-    "quote_next", "input:PASS-THROUGH(next)", "content_buffer_quote_next_keymap",
-    "This input mode sends the next key combo to the buffer, "+
-        "bypassing Conkeror's normal key handling.  The mode disengages "+
-        "after one key combo.");
-define_input_mode(
-    "quote", "input:PASS-THROUGH", "content_buffer_quote_keymap",
-    "This input mode sends all key combos to the buffer, "+
-        "bypassing Conkeror's normal key handling, until the "+
-        "Escape key is pressed.");
 
-// Input mode for the visible caret
-define_input_mode("caret", null, "content_buffer_caret_keymap");
-
-
-function content_buffer_update_input_mode_for_focus(buffer, force) {
+/**
+ * content_buffer_update_input_mode_for_focus is the controller for
+ * this input-mode system.  It sets the input-mode based on the focused
+ * element or lack thereof in the given buffer.  By default, it will
+ * not override input-modes such as caret-mode, which have been turned
+ * on explicitly by the user.  If force is true, then this function
+ * will even override those special input-modes.
+ */
+function content_buffer_update_input_mode_for_focus (buffer, force) {
     var mode = buffer.input_mode;
     var form_input_mode_enabled = (mode == "text_input_mode" ||
                                    mode == "textarea_input_mode" ||
@@ -69,7 +46,9 @@ function content_buffer_update_input_mode_for_focus(buffer, force) {
                                    mode == "checkbox_input_mode" ||
                                    mode == "richedit_input_mode");
 
-    if (force || form_input_mode_enabled || mode == "normal_input_mode") {
+    if (force || form_input_mode_enabled ||
+        mode == "normal_input_mode" || mode == null)
+    {
         let elem = buffer.focused_element;
 
         if (elem) {
@@ -91,7 +70,6 @@ function content_buffer_update_input_mode_for_focus(buffer, force) {
         }
 
         if (!input_mode_function) {
-
             let frame = buffer.focused_frame;
             let in_design_mode = false;
             if (frame && frame.document.designMode == "on")
@@ -116,7 +94,7 @@ function content_buffer_update_input_mode_for_focus(buffer, force) {
 
         if (input_mode_function) {
             if (!force &&
-                browser_prevent_automatic_form_focus_mode_enabled &&
+                browser_prevent_automatic_form_focus_mode_enabled && //XXX: breaks abstraction
                 !form_input_mode_enabled &&
                 (buffer.last_user_input_received == null ||
                  (Date.now() - buffer.last_user_input_received)
@@ -127,15 +105,70 @@ function content_buffer_update_input_mode_for_focus(buffer, force) {
                 input_mode_function(buffer, true);
             return;
         }
-
         normal_input_mode(buffer, true);
     }
 }
 
+//XXX: why does this command exist?  if the input-mode system works correctly
+//     then the user should never have to call this.
+interactive("content-buffer-update-input-mode-for-focus",
+    "This command provides a way for the user to force a reset of the"+
+    "input-mode system.",
+    function (I) {
+        content_buffer_update_input_mode_for_focus(I.buffer, true);
+    });
+
+
+/**
+ * Install the default input-mode system
+ */
+
+//XXX: bad to assume we can go into normal_input_mode. doesn't work if
+// you are in an input box on a frameset page, submit a form that
+// causes another frame to change location, but your input box keeps
+// focus.
+//
+// add_hook("content_buffer_location_change_hook", function (buf) { normal_input_mode(buf, true); });
+
+add_hook("content_buffer_location_change_hook",
+         content_buffer_update_input_mode_for_focus);
+
 add_hook("content_buffer_focus_change_hook",
-         function (buf) {
-             content_buffer_update_input_mode_for_focus(buf, false);
-         });
+         content_buffer_update_input_mode_for_focus);
+
+
+
+
+/*
+ * QUOTE INPUT MODES
+ */
+
+// Input modes for sending key events to gecko
+//
+// XXX: These quote modes are not rightly part of the system because
+// they are always invoked by the user.  Furthermore, they should be
+// generalized to work with any type of buffer.
+define_input_mode(
+    "quote_next", "input:PASS-THROUGH(next)", "content_buffer_quote_next_keymap",
+    "This input mode sends the next key combo to the buffer, "+
+        "bypassing Conkeror's normal key handling.  The mode disengages "+
+        "after one key combo.");
+define_input_mode(
+    "quote", "input:PASS-THROUGH", "content_buffer_quote_keymap",
+    "This input mode sends all key combos to the buffer, "+
+        "bypassing Conkeror's normal key handling, until the "+
+        "Escape key is pressed.");
+
+
+/*
+ * CARET INPUT MODE
+ */
+
+// Input mode for the visible caret
+//
+// XXX: caret-mode should be defined elsewhere, as it is not part of
+// the input-mode system.
+define_input_mode("caret", null, "content_buffer_caret_keymap");
 
 define_buffer_mode('caret_mode', 'CARET',
                    $enable = function(buffer) {
@@ -150,6 +183,7 @@ define_buffer_mode('caret_mode', 'CARET',
                        let sc = getFocusedSelCtrl(buffer);
                        sc.setCaretEnabled(false);
                        buffer.browser.focus();
+                       //XXX: dependency on the default input-mode system
                        content_buffer_update_input_mode_for_focus(buffer, true);
                    });
 
@@ -163,33 +197,11 @@ watch_pref(CARET_PREF, function() {
                }
            });
 
-interactive("content-buffer-update-input-mode-for-focus", null, function (I) {
-    content_buffer_update_input_mode_for_focus(I.buffer, true);
-});
 
-function minibuffer_input_mode_indicator(window) {
-    this.window = window;
-    this.hook_func = method_caller(this, this.update);
-    add_hook.call(window, "select_buffer_hook", this.hook_func);
-    add_hook.call(window, "current_buffer_input_mode_change_hook", this.hook_func);
-    this.update();
-}
 
-minibuffer_input_mode_indicator.prototype = {
-    update : function () {
-        var buf = this.window.buffers.current;
-        var mode = buf.input_mode;
-        if (mode)
-            this.window.minibuffer.element.className = "minibuffer-" + buf.input_mode.replace("_","-","g");
-    },
-    uninstall : function () {
-        remove_hook.call(window, "select_buffer_hook", this.hook_func);
-        remove_hook.call(window, "current_buffer_input_mode_change_hook", this.hook_func);
-    }
-};
-
-define_global_window_mode("minibuffer_input_mode_indicator", "window_initialize_hook");
-minibuffer_input_mode_indicator_mode(true);
+/*
+ * Browser Prevent Automatic Form Focus Mode
+ */
 
 // Milliseconds
 define_variable("browser_automatic_form_focus_window_duration", 20,

modules/content-buffer.js
@@ -1,14 +1,17 @@
 /**
  * (C) Copyright 2004-2007 Shawn Betts
- * (C) Copyright 2007-2008 John J. Foerch
+ * (C) Copyright 2007-2009 John J. Foerch
  * (C) Copyright 2007-2008 Jeremy Maitin-Shepard
  *
  * Use, modification, and distribution are subject to the terms specified in the
  * COPYING file.
 **/
 
+require("buffer.js");
 require("load-spec.js");
 
+//FIXME: circular dependency for browser_prevent_automatic_form_focus_mode,
+//       and content_buffer_update_input_mode_for_focus.
 require_later("content-buffer-input.js");
 
 define_variable("homepage", "chrome://conkeror-help/content/help.html",
@@ -91,12 +94,18 @@ function content_buffer (window, element) {
 	   WTF: What does that comment mean? Is PopupWindow an event? /Deniz
 	*/
 
-         this.browser.addEventListener("DOMPopupBlocked", function (event) {
-	     dumpln("Blocked popup: " + event.popupWindowURI.spec);
-             content_buffer_popup_blocked_hook.run(buffer, event);
-         }, true, false);
+        this.browser.addEventListener("DOMPopupBlocked", function (event) {
+	    dumpln("Blocked popup: " + event.popupWindowURI.spec);
+            content_buffer_popup_blocked_hook.run(buffer, event);
+        }, true, false);
 
-        normal_input_mode(this);
+        // XXX: This is needed to ensure we have a keymap at the
+        // instant the buffer is created, but before the page has been
+        // loaded in it.  It breaks heirarchy, as content_buffer
+        // should not have any business knowing about a specific
+        // input-mode system, so we'll see if we can figure out
+        // something more elegant.
+        content_buffer_update_input_mode_for_focus(this);
 
         this.ignore_initial_blank = true;
 
@@ -600,7 +609,7 @@ function define_page_mode(name, display_name) {
                            if (keymaps) {
                                remove_hook.call(buffer, "input_mode_change_hook",
                                                 page_mode_update_keymap);
-                               content_buffer_update_keymap_for_input_mode(buffer);
+                               buffer_update_keymap_for_input_mode(buffer);
                            }
                        },
                        $doc = doc);

commit 38bf0f5042ced23779e1e0c022409d2e5eda4106

fix issue189 (help buffer titles wrong in tab-bar)
Error was introduced in 1e1a96da42d12b107dc7c6c0041a355f9763c7a1, in which
tab-bar and new-tabs initialization was moved to create_buffer_early_hook.
The problem was that when create_buffer_early_hook runs, at the end of the
`buffer' constructor, the constructor for the subclass has not completed
running, so its fields are not filled in.  At this point in construction,
the field this.key_sequence is truly "undefined".

The solution was to put a title updater for the tab onto
create_buffer_hook, which runs when all constructors have completed.

modules/new-tabs.js
@@ -37,6 +37,7 @@ function tab_bar (window) {
     add_hook.call(window, "select_buffer_hook", tab_bar_select_buffer);
     add_hook.call(window, "create_buffer_early_hook", tab_bar_add_buffer);
     add_hook.call(window, "kill_buffer_hook", tab_bar_kill_buffer);
+    add_hook.call(window, "create_buffer_hook", tab_bar_update_buffer_title);
     add_hook.call(window, "buffer_title_change_hook", tab_bar_update_buffer_title);
     add_hook.call(window, "buffer_description_change_hook", tab_bar_update_buffer_title);
     //    add_hook.call(window, "buffer_favicon_change_hook", tab_bar_update_buffer_icon);
@@ -54,6 +55,7 @@ tab_bar.prototype.destroy = function () {
     remove_hook.call(this.window, "select_buffer_hook", tab_bar_select_buffer);
     remove_hook.call(this.window, "create_buffer_early_hook", tab_bar_add_buffer);
     remove_hook.call(this.window, "kill_buffer_hook", tab_bar_kill_buffer);
+    remove_hook.call(this.window, "create_buffer_hook", tab_bar_update_buffer_title);
     remove_hook.call(this.window, "buffer_title_change_hook", tab_bar_update_buffer_title);
     remove_hook.call(this.window, "buffer_description_change_hook", tab_bar_update_buffer_title);
     //    remove_hook.call(this.window, "buffer_favicon_change_hook", tab_bar_update_buffer_icon);

modules/tab-bar.js
@@ -20,6 +20,7 @@ function tab_bar (window) {
     add_hook.call(window, "select_buffer_hook", tab_bar_select_buffer);
     add_hook.call(window, "create_buffer_early_hook", tab_bar_add_buffer);
     add_hook.call(window, "kill_buffer_hook", tab_bar_kill_buffer);
+    add_hook.call(window, "create_buffer_hook", tab_bar_update_buffer_title);
     add_hook.call(window, "buffer_title_change_hook", tab_bar_update_buffer_title);
     add_hook.call(window, "buffer_description_change_hook", tab_bar_update_buffer_title);
     add_hook.call(window, "buffer_favicon_change_hook", tab_bar_update_buffer_icon);
@@ -32,6 +33,7 @@ tab_bar.prototype.destroy = function () {
     remove_hook.call(this.window, "select_buffer_hook", tab_bar_select_buffer);
     remove_hook.call(this.window, "create_buffer_early_hook", tab_bar_add_buffer);
     remove_hook.call(this.window, "kill_buffer_hook", tab_bar_kill_buffer);
+    remove_hook.call(this.window, "create_buffer_hook", tab_bar_update_buffer_title);
     remove_hook.call(this.window, "buffer_title_change_hook", tab_bar_update_buffer_title);
     remove_hook.call(this.window, "buffer_description_change_hook", tab_bar_update_buffer_title);
     remove_hook.call(this.window, "buffer_favicon_change_hook", tab_bar_update_buffer_icon);