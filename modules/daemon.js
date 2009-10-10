d0a9bd50e0837c937584cdceb77763b1dd948b9e
John Foerch <jjfoerch@earthlink.net>
2009-10-10 15:57:30 -0400

    daemon.js: cleanup and style changes
    daemon_quit_exits_p removed because it is not needed

--- modules/daemon.js
+++ modules/daemon.js
@@ -1,43 +1,37 @@
 /**
- * (C) Copyright 2007 John J. Foerch
+ * (C) Copyright 2007-2009 John J. Foerch
  * (C) Copyright 2007-2008 Jeremy Maitin-Shepard
  *
  * Use, modification, and distribution are subject to the terms specified in the
  * COPYING file.
 **/
 
-/*
- * Daemon Mode
- *
- *   This mode gets loaded into application scope.
- */
-
 require("mode.js");
 
-var daemon_quit_exits = true;
-
-function daemon_quit_exits_p () {
-    return daemon_quit_exits;
-}
+define_variable("daemon_quit_exits", true,
+    "This variable controls whether the `quit' command will exit "+
+    "the program or only close windows when daemon-mode is enabled. "+
+    "The default is true, meaning that the `quit' command will exit "+
+    "the program.");
 
 function daemon_quit_hook_fn () {
-    if (daemon_quit_exits_p ())
-        daemon_mode (-1);
+    if (daemon_quit_exits)
+        daemon_mode(-1);
 };
 
 define_global_mode("daemon_mode",
-                   function () { // enable
-                       var appStartup = Components.classes["@mozilla.org/toolkit/app-startup;1"]
-                           .getService(Components.interfaces.nsIAppStartup);
-                       appStartup.enterLastWindowClosingSurvivalArea();
-                       add_hook("quit_hook", daemon_quit_hook_fn);
-                   },
-                   function () { // disable
-                       var appStartup = Cc["@mozilla.org/toolkit/app-startup;1"]
-                           .getService(Ci.nsIAppStartup);
-                       appStartup.exitLastWindowClosingSurvivalArea ();
-                       remove_hook("quit_hook", daemon_quit_hook_fn);
-                   });
+    function () { // enable
+        Cc["@mozilla.org/toolkit/app-startup;1"]
+            .getService(Ci.nsIAppStartup)
+            .enterLastWindowClosingSurvivalArea();
+        add_hook("quit_hook", daemon_quit_hook_fn);
+    },
+    function () { // disable
+        Cc["@mozilla.org/toolkit/app-startup;1"]
+            .getService(Ci.nsIAppStartup)
+            .exitLastWindowClosingSurvivalArea();
+        remove_hook("quit_hook", daemon_quit_hook_fn);
+    });
 
 require_later("command-line.js");
 
@@ -45,6 +39,6 @@ call_after_load("command-line.js", function () {
         command_line_handler("daemon", true, function () {
                 daemon_mode(true);
                 var window = make_chrome_window(conkeror_chrome_uri);
-                window.setTimeout(function() { window.close(); }, 0);
+                window.setTimeout(function () { window.close(); }, 0);
             });
     });
