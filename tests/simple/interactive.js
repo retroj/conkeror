57a6f91196f9d1da388136d9bea5bafb9bb9f40b
John Foerch <jjfoerch@earthlink.net>
2009-10-11 13:46:16 -0400

    tests/simple/interactive.js: update for new input system
    call_interactively is a coroutine now.

--- tests/simple/interactive.js
+++ tests/simple/interactive.js
@@ -5,7 +5,7 @@ require('walnut.js');
       test_call_interactively_1: function () {
           let a = 0;
           interactive('test1', null, function () { a = 1; });
-          call_interactively({}, 'test1');
+          co_call(call_interactively({}, 'test1'));
           assert_equals(a, 1);
       }
   };
