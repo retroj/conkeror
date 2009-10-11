b5c6c6ba6507f5189703a03077f414020e66f077
John Foerch <jjfoerch@earthlink.net>
2009-10-11 13:47:48 -0400

    tests/simple/webjump.js: fixup and add some tests
    commented out obsolete wikipedia-webjump tests.  they need to be rewritten.

--- tests/simple/webjump.js
+++ tests/simple/webjump.js
@@ -1,8 +1,7 @@
 
 require('walnut.js');
 
-// webjump tests
-{ let suite = {
+{ let suite1 = {
       suite_setup: function () {
           this.real_webjumps = webjumps;
           conkeror.webjumps = {};
@@ -17,19 +16,38 @@ require('walnut.js');
       test_webjump_2: function () {
           define_webjump("test2", "http://www.example.com/with/a/path");
           assert_equals(webjumps["test2"].handler(), "http://www.example.com/with/a/path");
-      },
-      test_string_webjump_optional_arg_3: function () {
+      }
+  };
+  let suite2 = {
+      suite_setup: function () {
+          this.real_webjumps = webjumps;
+          conkeror.webjumps = {};
           define_webjump("test1", "http://www.example.com/search?term=%s");
+      },
+      suite_teardown: function () {
+          conkeror.webjumps = this.real_webjumps;
+      },
+      test_alternative_was_set: function () {
+          assert_equals(webjumps["test1"].alternative,
+                        "http://www.example.com/");
+      },
+      test_string_webjump_optional_arg_1: function () {
           assert_equals(webjumps["test1"].handler("foo"),
                         "http://www.example.com/search?term=foo");
-          assert_equals(webjumps["test1"].handler(),
+      },
+      test_string_webjump_optional_arg_2: function () {
+          assert_equals(get_url_or_webjump("test1"),
                         "http://www.example.com/");
+      },
+      test_string_webjump_optional_arg_3: function () {
           assert_equals(webjumps["test1"].argument, "optional");
       }
   };
-  walnut_run(suite);
+  walnut_run(suite1);
+  walnut_run(suite2);
 }
 
+/*
 require('wikipedia-webjumps.js');
 
 { let suite = {
@@ -58,3 +76,4 @@ require('wikipedia-webjumps.js');
   };
   walnut_run(suite);
 }
+*/
