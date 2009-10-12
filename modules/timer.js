866599329a33228e12615824b38ee19c39dd6504
John Foerch <jjfoerch@earthlink.net>
2009-10-12 15:14:01 -0400

    whitespace

--- modules/timer.js
+++ modules/timer.js
@@ -5,29 +5,30 @@
  * COPYING file.
 **/
 
-function timer_callback(callback) {
+function timer_callback (callback) {
     this.callback = callback;
 }
 timer_callback.prototype = {
     QueryInterface: XPCOMUtils.generateQI([Ci.nsITimerCallback]),
 
-    notify : function(timer) {
+    notify: function (timer) {
         this.callback.call(null, timer);
     }
 };
-function call_after_timeout(callback, timeout) {
+
+function call_after_timeout (callback, timeout) {
     var timer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
     timer.initWithCallback(new timer_callback(callback), timeout, Ci.nsITimer.TYPE_ONE_SHOT);
     return timer;
 }
 
-function call_at_interval(callback, interval) {
+function call_at_interval (callback, interval) {
     var timer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
     timer.initWithCallback(new timer_callback(callback), interval, Ci.nsITimer.TYPE_REPEATING_SLACK);
     return timer;
 }
 
-function call_at_precise_interval(callback, interval) {
+function call_at_precise_interval (callback, interval) {
     var timer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
     timer.initWithCallback(new timer_callback(callback), interval, Ci.nsITimer.TYPE_REPEATING_PRECISE);
     return timer;
