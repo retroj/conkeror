02cd2db96d21b85b009c76eb50b7ab6153b144b6
John Foerch <jjfoerch@earthlink.net>
2009-11-05 14:00:46 -0500

    youporn.js: removed
    Youporn-mode doesn't work, but even if it did, it serves no purpose.
    It uses Conkeror's media-scraper system to find the url of the flv
    file for an embedded media player.  However, youporn.com already
    provides a hyperlink to download that flv file.  Additionally, even
    if you do want to use the media-scraper system instead of the hyperlink
    to download the file, Conkeror's built-in media_scrape_default finds
    the url just fine.  In summary and conclusion, there is no reason,
    in triplicate, for this mode to exist.

--- defaults/preferences/default-modules.js
+++ defaults/preferences/default-modules.js
@@ -57,7 +57,6 @@ pref("conkeror.load.page-modes/google-calendar", 1);
 pref("conkeror.load.page-modes/google-reader", 1);
 pref("conkeror.load.page-modes/google-video", 1);
 pref("conkeror.load.page-modes/google-maps", 1);
-pref("conkeror.load.page-modes/youporn", 1);
 pref("conkeror.load.page-modes/dailymotion", 1);
 pref("conkeror.load.page-modes/gmail", 0);
 pref("conkeror.load.page-modes/xkcd", 1);
