3fbcd0da86c7141894bcab7836bb1fe09689d7fc
John Foerch <jjfoerch@earthlink.net>
2009-11-08 12:19:39 -0500

    whitespace

--- modules/minibuffer-read-file.js
+++ modules/minibuffer-read-file.js
@@ -7,6 +7,39 @@
 **/
 
 require("io.js");
+require("minibuffer-completion.js");
+
+
+function file_path_completer () {
+    return function (input, pos, conservative) {
+        var f = Cc["@mozilla.org/file/local;1"]
+            .createInstance(Ci.nsILocalFile);
+        var ents = [];
+        var dir;
+        try {
+            f.initWithPath(input);
+            if(f.exists() && f.isDirectory())
+                dir = f;
+            else
+                dir = f.parent;
+            if (!dir.exists())
+                return null;
+            var iter = dir.directoryEntries;
+            while (iter.hasMoreElements()) {
+                var e = iter.getNext().QueryInterface(Ci.nsIFile);
+                ents.push(e.path);
+            }
+        } catch(e) {
+            return null;
+        }
+        function id (x) { return x; };
+        return prefix_completer($completions = ents,
+                                $get_string  = id,
+                                $get_description = id,
+                                $get_value = id)(input, pos, conservative);
+    };
+}
+
 
 /* keywords: $prompt, $initial_value, $history, $completer, $auto_complete */
 minibuffer.prototype.read_file_path = function () {
@@ -35,10 +68,9 @@ minibuffer.prototype.read_file_check_overwrite = function () {
     keywords(arguments);
     var initial_value = arguments.$initial_value;
     do {
-        var path = yield this.read_file_path(forward_keywords(arguments), $initial_value = initial_value);
-
+        var path = yield this.read_file_path(forward_keywords(arguments),
+                                             $initial_value = initial_value);
         var file = make_file(path);
-
         if (file.exists()) {
             var overwrite = yield this.read_yes_or_no($prompt = "Overwrite existing file " + path + "?");
             if (!overwrite) {
@@ -49,31 +81,3 @@ minibuffer.prototype.read_file_check_overwrite = function () {
         yield co_return(file);
     } while (true);
 };
-
-function file_path_completer() {
-    return function(input, pos, conservative) {
-        var f = Cc["@mozilla.org/file/local;1"].createInstance(Ci.nsILocalFile);
-        var ents = [];
-        var dir;
-        try {
-            f.initWithPath(input);
-            if(f.exists() && f.isDirectory())
-                dir = f;
-            else
-                dir = f.parent;
-            if(!dir.exists()) return null;
-            var iter = dir.directoryEntries;
-            while(iter.hasMoreElements()) {
-                var e = iter.getNext().QueryInterface(Ci.nsIFile);
-                ents.push(e.path);
-            }
-        } catch(e) {
-            return null;
-        }
-        function id(x) { return x; };
-        return prefix_completer($completions = ents,
-                                $get_string  = id,
-                                $get_description = id,
-                                $get_value = id)(input, pos, conservative);
-    };
-}
