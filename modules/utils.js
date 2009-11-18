retroj/conkeror

commit 6c90a662949ed2a16b77e06adbcc78aa2d3d58e3

load-spec refactoring
load_spec is now a normal datatype.  All load-specs are `instanceof
load_spec'.  Specifically, strings are no longer a type of load_spec.
Webjumps are now resolved at construction-time of the load_spec,
which allows webjumps to return load_spec parameters.  This makes
it possible (more elegantly with the earlier implementation) to make
webjumps that POST.

make_post_data: new util function for defining POST webjumps

modules/utils.js
@@ -1177,6 +1177,8 @@ function send_http_request(lspec) {
     // following keywords?
     keywords(arguments, $user = undefined, $password = undefined,
              $override_mime_type = undefined, $headers = undefined);
+    if (! (lspec instanceof load_spec))
+        lspec = load_spec(lspec);
     var req = xml_http_request();
     var cc = yield CONTINUATION;
     var aborting = false;
@@ -1558,4 +1560,22 @@ function dom_node_flash (node, cssclass) {
             dom_remove_class(node, cssclass);
         },
         400);
+}
+
+
+/**
+ * data is an an alist (array of 2 element arrays) where each pair is a key
+ * and a value.
+ *
+ * The return type is a mime input stream that can be passed as postData to
+ * nsIWebNavigation.loadURI.  In terms of Conkeror's API, the return value
+ * of this function is of the correct type for the `post_data' field of a
+ * load_spec.
+ */
+function make_post_data (data) {
+    data = [(encodeURIComponent(pair[0])+'='+encodeURIComponent(pair[1]))
+            for each (pair in data)].join('&');
+    data = string_input_stream(data);
+    return mime_input_stream(
+        data, [["Content-Type", "application/x-www-form-urlencoded"]]);
 }

modules/clicks-in-new-buffer.js
@@ -49,7 +49,7 @@ function open_link_in_new_buffer(event) {
     event.preventDefault();
     if (clicks_in_new_buffer_ev_stop_prop)
         event.stopPropagation();
-    let spec = element_get_load_spec(anchor);
+    let spec = load_spec(anchor);
     // FIXME Is there a better way to get the window?
     let window = window_watcher.activeWindow;
     let buffer = window.buffers.current;

modules/commands.js
@@ -513,9 +513,7 @@ interactive("focus", null,
 interactive("save", null, function (I) {
     var element = yield read_browser_object(I);
 
-    var spec = element_get_load_spec(element);
-    if (spec == null)
-        throw interactive_error("Element has no associated URI");
+    var spec = load_spec(element);
 
     var panel;
     panel = create_info_panel(I.window, "download-panel",
@@ -567,9 +565,7 @@ interactive("view-source", null,
 interactive("shell-command-on-url", null, function (I) {
     var cwd = I.cwd;
     var element = yield read_browser_object(I);
-    var spec = element_get_load_spec(element);
-    if (spec == null)
-        throw interactive_error("Unable to obtain URI from element");
+    var spec = load_spec(element);
 
     var uri = load_spec_uri_string(spec);
 
@@ -598,9 +594,7 @@ interactive("shell-command-on-file", null, function (I) {
     var cwd = I.cwd;
     var element = yield read_browser_object(I);
 
-    var spec = element_get_load_spec(element);
-    if (spec == null)
-        throw interactive_error("Unable to obtain URI from element");
+    var spec = load_spec(element);
 
     var uri = load_spec_uri_string(spec);
 
@@ -628,9 +622,7 @@ interactive("shell-command-on-file", null, function (I) {
 
 interactive("bookmark", null, function (I) {
     var element = yield read_browser_object(I);
-    var spec = element_get_load_spec(element);
-    if (!spec)
-        throw interactive_error("Element has no associated URI");
+    var spec = load_spec(element);
     var uri_string = load_spec_uri_string(spec);
     var panel;
     panel = create_info_panel(I.window, "bookmark-panel",
@@ -650,8 +642,8 @@ interactive("bookmark", null, function (I) {
 interactive("save-page", null, function (I) {
     check_buffer(I.buffer, content_buffer);
     var element = yield read_browser_object(I);
-    var spec = element_get_load_spec(element);
-    if (!spec || !load_spec_document(spec))
+    var spec = load_spec(element);
+    if (!load_spec_document(spec))
         throw interactive_error("Element is not associated with a document.");
     var suggested_path = suggest_save_path_from_file_name(suggest_file_name(spec), I.buffer);
 
@@ -678,9 +670,9 @@ interactive("save-page", null, function (I) {
 interactive("save-page-as-text", null, function (I) {
     check_buffer(I.buffer, content_buffer);
     var element = yield read_browser_object(I);
-    var spec = element_get_load_spec(element);
+    var spec = load_spec(element);
     var doc;
-    if (!spec || !(doc = load_spec_document(spec)))
+    if (!(doc = load_spec_document(spec)))
         throw interactive_error("Element is not associated with a document.");
     var suggested_path = suggest_save_path_from_file_name(suggest_file_name(spec, "txt"), I.buffer);
 
@@ -707,9 +699,9 @@ interactive("save-page-as-text", null, function (I) {
 interactive("save-page-complete", null, function (I) {
     check_buffer(I.buffer, content_buffer);
     var element = yield read_browser_object(I);
-    var spec = element_get_load_spec(element);
+    var spec = load_spec(element);
     var doc;
-    if (!spec || !(doc = load_spec_document(spec)))
+    if (!(doc = load_spec_document(spec)))
         throw interactive_error("Element is not associated with a document.");
     var suggested_path = suggest_save_path_from_file_name(suggest_file_name(spec), I.buffer);
 
@@ -742,13 +734,10 @@ interactive("save-page-complete", null, function (I) {
 function view_as_mime_type (I, target) {
     I.target = target;
     var element = yield read_browser_object(I);
-    var spec = element_get_load_spec(element);
+    var spec = load_spec(element);
 
     if (target == null)
         target = FOLLOW_CURRENT_FRAME;
-
-    if (!spec)
-        throw interactive_error("Element is not associated with a URI");
 
     if (!can_override_mime_type_for_uri(load_spec_uri(spec)))
         throw interactive_error("Overriding the MIME type is not currently supported for non-HTTP URLs.");

modules/element.js
@@ -259,7 +259,7 @@ function browser_object_follow(buffer, target, elem)
     if (elem instanceof Ci.nsILocalFile)
         elem = elem.path;
 
-    var no_click = (is_load_spec(elem) ||
+    var no_click = ((elem instanceof load_spec) ||
                     (elem instanceof Ci.nsIDOMWindow) ||
                     (elem instanceof Ci.nsIDOMHTMLFrameElement) ||
                     (elem instanceof Ci.nsIDOMHTMLIFrameElement) ||
@@ -280,11 +280,7 @@ function browser_object_follow(buffer, target, elem)
         return;
     }
 
-    var spec = element_get_load_spec(elem);
-    if (spec == null) {
-        throw interactive_error("Element has no associated URL");
-        return;
-    }
+    var spec = load_spec(elem);
 
     if (load_spec_uri_string(spec).match(/^\s*javascript:/)) {
         // it is nonsensical to follow a javascript url in a different
@@ -338,70 +334,6 @@ function browser_follow_link_with_click(buffer, elem, x, y) {
     evt.initMouseEvent("click", true, true, view, 1, x, y, 0, 0, /*ctrl*/ 0, /*event.altKey*/0,
                        /*event.shiftKey*/ 0, /*event.metaKey*/ 0, 0, null);
     elem.dispatchEvent(evt);
-}
-
-function element_get_load_spec(elem) {
-
-    if (is_load_spec(elem))
-        return elem;
-
-    var spec = null;
-
-    if (elem instanceof Ci.nsIDOMWindow)
-        spec = load_spec({document: elem.document});
-
-    else if (elem instanceof Ci.nsIDOMHTMLFrameElement ||
-             elem instanceof Ci.nsIDOMHTMLIFrameElement)
-        spec = load_spec({document: elem.contentDocument});
-
-    else {
-        var url = null;
-        var title = null;
-
-        if (elem instanceof Ci.nsIDOMHTMLAnchorElement ||
-            elem instanceof Ci.nsIDOMHTMLAreaElement ||
-            elem instanceof Ci.nsIDOMHTMLLinkElement) {
-            if (!elem.hasAttribute("href"))
-                return null; // nothing can be done, as no nesting within these elements is allowed
-            url = elem.href;
-            title = elem.title || elem.textContent;
-        }
-        else if (elem instanceof Ci.nsIDOMHTMLImageElement) {
-            url = elem.src;
-            title = elem.title || elem.alt;
-        }
-        else {
-            var node = elem;
-            while (node && !(node instanceof Ci.nsIDOMHTMLAnchorElement))
-                node = node.parentNode;
-            if (node) {
-                if (node.hasAttribute("href"))
-                    url = node.href;
-                else
-                    node = null;
-            }
-            if (!node) {
-                // Try simple XLink
-                node = elem;
-                while (node) {
-                    if (node.nodeType == Ci.nsIDOMNode.ELEMENT_NODE) {
-                        url = node.getAttributeNS(XLINK_NS, "href");
-                        break;
-                    }
-                    node = node.parentNode;
-                }
-                if (url)
-                    url = makeURLAbsolute(node.baseURI, url);
-                title = node.title || node.textContent;
-            }
-        }
-        if (url && url.length > 0) {
-            if (title && title.length == 0)
-                title = null;
-            spec = load_spec({uri: url, source_frame: elem.ownerDocument.defaultView, title: title});
-        }
-    }
-    return spec;
 }
 
 
@@ -468,7 +400,10 @@ function element_get_operation_label(element, op_name, suffix) {
 
 function browser_element_copy(buffer, elem)
 {
-    var spec = element_get_load_spec(elem);
+    var spec;
+    try {
+       spec = load_spec(elem);
+    } catch (e) {}
     var text = null;
     if (spec)
         text = load_spec_uri_string(spec);
@@ -500,11 +435,7 @@ function browser_object_view_source(buffer, target, elem)
 {
     if (view_source_use_external_editor || view_source_function)
     {
-        var spec = element_get_load_spec(elem);
-        if (spec == null) {
-            throw interactive_error("Element has no associated URL");
-            return;
-        }
+        var spec = load_spec(elem);
 
         let [file, temp] = yield download_as_temporary(spec,
                                                        $buffer = buffer,
@@ -559,11 +490,7 @@ function view_source_new_window (I) {
 
 
 function browser_element_shell_command(buffer, elem, command) {
-    var spec = element_get_load_spec(elem);
-    if (spec == null) {
-        throw interactive_error("Element has no associated URL");
-        return;
-    }
+    var spec = load_spec(elem);
     yield download_as_temporary(spec,
                                 $buffer = buffer,
                                 $shell_command = command,

modules/extensions/adblockplus.js
@@ -28,9 +28,7 @@ interactive("adblockplus-add", "Add a pattern to Adblock Plus.",
             function (I) {
     var element = yield read_browser_object(I);
 
-    var spec = element_get_load_spec(element);
-    if (spec == null)
-        throw interactive_error("Element has no associated URI");
+    var spec = load_spec(element);
 
     var pattern = yield I.minibuffer.read_url(
         $prompt = "Adblock:",

modules/hints.js
@@ -348,8 +348,10 @@ function hints_url_panel(hints, window) {
     p.update = function() {
 	url_value.value = "";
 	if (hints.manager && hints.manager.last_selected_hint) {
-            var spec = element_get_load_spec(
-                hints.manager.last_selected_hint.elem);
+            var spec;
+            try {
+                spec = load_spec(hints.manager.last_selected_hint.elem);
+            } catch (e) {}
             if (spec) {
                 var uri = load_spec_uri_string(spec);
                 if (uri) url_value.value = uri;

modules/load-spec.js
@@ -52,11 +52,85 @@
 
 require("webjump.js");
 
-function load_spec(x) { x.__proto__ = load_spec.prototype; return x; }
+function load_spec_from_element (elem) {
+    var spec = {};
+    if (elem instanceof Ci.nsIDOMWindow)
+        spec.document = elem.document;
 
-function is_load_spec(x) {
-    return (typeof(x) == "string") || (x instanceof load_spec);
+    else if (elem instanceof Ci.nsIDOMHTMLFrameElement ||
+             elem instanceof Ci.nsIDOMHTMLIFrameElement)
+        spec.document = elem.contentDocument;
+
+    else {
+        var url = null;
+        var title = null;
+
+        if (elem instanceof Ci.nsIDOMHTMLAnchorElement ||
+            elem instanceof Ci.nsIDOMHTMLAreaElement ||
+            elem instanceof Ci.nsIDOMHTMLLinkElement) {
+            if (elem.hasAttribute("href"))
+                url = elem.href;
+            title = elem.title || elem.textContent;
+        }
+        else if (elem instanceof Ci.nsIDOMHTMLImageElement) {
+            url = elem.src;
+            title = elem.title || elem.alt;
+        }
+        else {
+            var node = elem;
+            while (node && !(node instanceof Ci.nsIDOMHTMLAnchorElement))
+                node = node.parentNode;
+            if (node) {
+                if (node.hasAttribute("href"))
+                    url = node.href;
+                else
+                    node = null;
+            }
+            if (!node) {
+                // Try simple XLink
+                node = elem;
+                while (node) {
+                    if (node.nodeType == Ci.nsIDOMNode.ELEMENT_NODE) {
+                        url = node.getAttributeNS(XLINK_NS, "href");
+                        break;
+                    }
+                    node = node.parentNode;
+                }
+                if (url)
+                    url = makeURLAbsolute(node.baseURI, url);
+                title = node.title || node.textContent;
+            }
+        }
+        if (url && url.length > 0) {
+            if (title && title.length == 0)
+                title = null;
+            spec.uri = url;
+            spec.source_frame = elem.ownerDocument.defaultView;
+            spec.title = title;
+        }
+    }
+    return spec;
 }
+
+function load_spec (x) {
+    var spec;
+    if (typeof(x) == "string")
+        x = get_url_or_webjump(x);
+    if (typeof(x) == "string")
+        spec = { uri: x };
+    else if ((x instanceof Ci.nsIDOMNode) ||
+             (x instanceof Ci.nsIDOMWindow))
+    {
+        spec = load_spec_from_element(x);
+    } else if (typeof(x) == "object") {
+        spec = x;
+    }
+    if (! load_spec_uri_string(spec))
+        throw new Error("Invalid load-spec");
+    spec.__proto__ = load_spec.prototype;
+    return spec;
+}
+load_spec.prototype = {};
 
 function load_spec_document(x) {
     return x.document;
@@ -71,12 +145,10 @@ function load_spec_title(x) {
 }
 
 function load_spec_mime_type(x) {
-    if (typeof(x) == "object") {
-        if (x.mime_type)
-            return x.mime_type;
-        if (x.document)
-            return x.document.contentType || "application/octet-stream";
-    }
+    if (x.mime_type)
+        return x.mime_type;
+    if (x.document)
+        return x.document.contentType || "application/octet-stream";
     return mime_type_from_uri(load_spec_uri(x));
 }
 
@@ -171,9 +243,7 @@ function load_spec_source_frame(x) {
     return null;
 }
 
-function load_spec_uri_string_bypass_webjump(x) {
-    if (typeof(x) == "string")
-       return x;
+function load_spec_uri_string(x) {
     if (x.uri)
         return x.uri;
     if (x.document && x.document.defaultView)
@@ -181,12 +251,6 @@ function load_spec_uri_string_bypass_webjump(x) {
     if (x.document)
         return x.document.documentURI;
     return null;
-}
-
-function load_spec_uri_string(x) {
-    if (typeof(x) == "string")
-        x = get_url_or_webjump(x);
-    return load_spec_uri_string_bypass_webjump(x);
 }
 
 function load_spec_uri(x) {
@@ -213,9 +277,9 @@ function load_spec_default_shell_command(x) {
 
 /* Target can be either a content_buffer or an nsIWebNavigation */
 function apply_load_spec(target, spec) {
-    if (typeof(spec) == "string")
-        spec = get_url_or_webjump(spec);
-    var uri = load_spec_uri_string_bypass_webjump(spec);
+    if (! (spec instanceof load_spec))
+        spec = load_spec(spec);
+    var uri = load_spec_uri_string(spec);
     var flags = load_spec_flags(spec);
     var referrer = load_spec_referrer(spec);
     var post_data = load_spec_post_data(spec);

modules/media.js
@@ -79,8 +79,14 @@ define_browser_object_class(
 
         let completer = all_word_completer(
             $completions = media,
-            $get_string = function (x) load_spec_uri_string(x),
-            $get_description = function (x) load_spec_title(x) || "");
+            $get_string = function (x) {
+                x = load_spec(x);
+                return load_spec_uri_string(x);
+            },
+            $get_description = function (x) {
+                x = load_spec(x);
+                return load_spec_title(x) || "";
+            });
 
         let result = yield I.buffer.window.minibuffer.read(
             $prompt = prompt,

commit 65336c6e46a32eebf23a8d6cea756a58b33bbc1d

Fix editing source from help buffer.
This was probably broken by commit 6c90a66 (load-spec refactoring, 2009-02-03).

modules/utils.js
@@ -378,7 +378,8 @@ source_code_reference.prototype = {
     },
 
     open_in_editor : function() {
-        yield open_with_external_editor(this.best_uri, $line = this.line_number);
+        yield open_with_external_editor(load_spec(this.best_uri),
+                                        $line = this.line_number);
     }
 };
 