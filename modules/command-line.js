retroj/conkeror

commit c0c519ac2e3af3ce05ee0d3c671b85b1e929b4ad

new input system
  This patch replaces keyboard.js with keymap.js and input.js, introducing
a substantial rewrite of the input system which addresses several
long-standing bugs and feature requests, and puts the framework in place
for others.

http://bugs.conkeror.org/issue159  $fallthrough and keydown events

  Fixed, by a new mechanism for explicitly allowing keydown and keyup
events.  Function `define_fallthrough' implements this.

http://bugs.conkeror.org/issue146  'f' and 'C-u f' don't work on Slashdot articles

  Fixed.

http://bugs.conkeror.org/issue92  M-x cannot call prefix commands

  Fixed.

http://bugs.conkeror.org/issue22  Mouse clicks integrated into key binding system

  Preliminary support for mouse bindings.  Completing this will entail
improving the input-mode system, to set the proper keymaps for mouse
actions.

modules/command-line.js
@@ -1,5 +1,5 @@
 /**
- * (C) Copyright 2007 John J. Foerch
+ * (C) Copyright 2007-2009 John J. Foerch
  * (C) Copyright 2007-2008 Jeremy Maitin-Shepard
  *
  * Use, modification, and distribution are subject to the terms specified in the
@@ -90,7 +90,7 @@ command_line_param_handler("f", true, function (command, ctx) {
         // hack to make sure we send this command to a window
         ctx.window = get_recent_conkeror_window();
         ctx.buffer = ctx.window.buffers.current;
-        call_interactively(ctx, command);
+        co_call(call_interactively(ctx, command));
     });
 
 command_line_param_handler("l", false, function (path, ctx) {

contrib/xrev/content/xrev.js
@@ -1,5 +1,5 @@
 /**
- * (C) Copyright 2008 John J. Foerch
+ * (C) Copyright 2008-2009 John J. Foerch
  *
  * Use, modification, and distribution are subject to the terms specified in the
  * COPYING file.
@@ -102,6 +102,8 @@ function key_event_handler (event) {
           for each (k in key_event_props)].join(''));
     dumpln(combo);
 }
+window.addEventListener("keydown", key_event_handler, true /* capture */,
+                        false /* ignore untrusted events */);
 window.addEventListener("keypress", key_event_handler, true /* capture */,
                         false /* ignore untrusted events */);
 

modules/bindings/default/content-buffer/caret.js
@@ -23,7 +23,6 @@ define_key(content_buffer_caret_keymap, "C-right", "caret-forward-word");
 define_key(content_buffer_caret_keymap, "M-right", "caret-forward-word");
 define_key(content_buffer_caret_keymap, "M-w", "cmd_copy");
 
-// 101 keys
 define_key(content_buffer_caret_keymap, "home", "caret-beginning-of-line");
 define_key(content_buffer_caret_keymap, "end", "caret-end-of-line");
 define_key(content_buffer_caret_keymap, "S-home", "cmd_selectBeginLine");

modules/bindings/default/content-buffer/checkbox.js
@@ -9,6 +9,7 @@ require("bindings/default/content-buffer/form.js");
 
 /* Keymap for checkboxes and radiobuttons */
 define_keymap("content_buffer_checkbox_keymap", $parent = content_buffer_form_keymap);
+define_fallthrough(content_buffer_checkbox_keymap, match_checkbox_keys);
 
 define_key(content_buffer_checkbox_keymap, "space", null, $fallthrough);
 

modules/bindings/default/content-buffer/quote.js
@@ -1,5 +1,6 @@
 /**
  * (C) Copyright 2008 Jeremy Maitin-Shepard
+ * (C) Copyright 2009 John J. Foerch
  *
  * Use, modification, and distribution are subject to the terms specified in the
  * COPYING file.
@@ -8,8 +9,10 @@
 define_keymap("content_buffer_quote_next_keymap");
 define_keymap("content_buffer_quote_keymap");
 
+define_fallthrough(content_buffer_quote_next_keymap, match_any_key);
 define_key(content_buffer_quote_next_keymap, match_any_key, "content-buffer-update-input-mode-for-focus", $fallthrough);
 
+define_fallthrough(content_buffer_quote_keymap, match_not_escape_key);
 define_key(content_buffer_quote_keymap, "escape", "content-buffer-update-input-mode-for-focus");
 define_key(content_buffer_quote_keymap, "M-escape", "content-buffer-update-input-mode-for-focus");
 define_key(content_buffer_quote_keymap, match_any_key, null, $fallthrough);

modules/bindings/default/content-buffer/richedit.js
@@ -1,5 +1,6 @@
 /**
  * (C) Copyright 2008 Jeremy Maitin-Shepard
+ * (C) Copyright 2009 John J. Foerch
  *
  * Use, modification, and distribution are subject to the terms specified in the
  * COPYING file.
@@ -8,3 +9,4 @@
 require("bindings/default/content-buffer/textarea.js");
 
 define_keymap("content_buffer_richedit_keymap", $parent = content_buffer_textarea_keymap);
+define_fallthrough(content_buffer_richedit_keymap, match_text_keys);

modules/bindings/default/content-buffer/select.js
@@ -29,7 +29,7 @@ define_key(content_buffer_select_keymap, "end", null, $fallthrough);
 
 define_key(content_buffer_select_keymap, "return", null, $fallthrough);
 
-define_key(content_buffer_select_keymap, match_any_unmodified_key, null, $fallthrough);
+define_key(content_buffer_select_keymap, match_any_unmodified_character, null, $fallthrough);
 //define_key(content_buffer_select_keymap, match_any_key, null, $fallthrough);
 
 define_key(content_buffer_select_keymap, "escape", "unfocus"); // leave text input mode

modules/bindings/default/content-buffer/text.js
@@ -1,6 +1,6 @@
 /**
  * (C) Copyright 2004-2007 Shawn Betts
- * (C) Copyright 2007-2008 John J. Foerch
+ * (C) Copyright 2007-2009 John J. Foerch
  * (C) Copyright 2007-2008 Jeremy Maitin-Shepard
  * (C) Copyright 2009 Deniz Dogan
  *
@@ -11,6 +11,9 @@
 require("bindings/default/content-buffer/form.js");
 
 define_keymap("content_buffer_text_keymap", $parent = content_buffer_form_keymap);
+define_fallthrough(content_buffer_text_keymap, match_text_keys);
+
+define_key(content_buffer_text_keymap, match_any_unmodified_character, null, $fallthrough);
 
 // Movement
 define_key(content_buffer_text_keymap, "C-a", "beginning-of-line");
@@ -65,6 +68,3 @@ define_key(content_buffer_text_keymap, "M-u", "upcase-word");
 define_key(content_buffer_text_keymap, "M-c", "capitalize-word");
 define_key(content_buffer_text_keymap, "C-i", "edit-current-field-in-external-editor");
 define_key(content_buffer_text_keymap, "C-t", "transpose-chars");
-
-// This must be defined last so it doesn't capture any "real" bindings.
-define_key(content_buffer_text_keymap, match_any_unmodified_key, null, $fallthrough);

commit 6b5a0fc4299a0b35661327887f38d4a6a0bfed06

Fix command line +u
Broken in c0c519a (new input system, 2009-10-08).

modules/command-line.js
@@ -107,7 +107,7 @@ command_line_handler("u", false, function (ctx) {
             ctx.window = get_recent_conkeror_window();
             ctx.buffer = ctx.window.buffers.current;
         }
-        call_interactively(ctx, "universal-argument");
+        co_call(call_interactively(ctx, "universal-argument"));
     });
 
 function handle_command_line (cmdline) {