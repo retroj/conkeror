cf37e8b454a8478854778c8dd27700cdc0be247e
John Foerch <jjfoerch@earthlink.net>
2010-02-25 15:41:00 -0500

    dom-inspector.js: whitespace, style, 'require' fix

--- modules/extensions/dom-inspector.js
+++ modules/extensions/dom-inspector.js
@@ -5,46 +5,49 @@
  * COPYING file.
 **/
 
-require("window.js");
-require("utils.js");
+require("extension.js");
 
 if (!extension_is_enabled("inspector@mozilla.org"))
     throw skip_module_load;
 
-function open_dom_inspector() {
+function open_dom_inspector () {
     make_chrome_window("chrome://inspector/content/");
 }
 
-function inspect_dom_document(document) {
+function inspect_dom_document (document) {
     make_chrome_window("chrome://inspector/content/", document);
 }
 
-function inspect_dom_node(node) {
+function inspect_dom_node (node) {
     make_chrome_window("chrome://inspector/content/", node);
 }
 
-function inspect_javascript_object(obj) {
+function inspect_javascript_object (obj) {
     make_chrome_window("chrome://inspector/content/object.xul", obj);
 }
 
-interactive("inspector", "Open the DOM Inspector in a new window.",
-            function (I) { open_dom_inspector(); });
+interactive("inspector",
+    "Open the DOM Inspector in a new window.",
+    open_dom_inspector);
 
-interactive("inspect-chrome", "Inspect the chrome document for the current Conkeror window.",
-            function (I) {inspect_dom_document(I.window.document);});
+interactive("inspect-chrome",
+    "Inspect the chrome document for the current Conkeror window.",
+    function (I) { inspect_dom_document(I.window.document); });
 
-interactive("inspect-page", "Inspect the current content document.",
-            function (I) {inspect_dom_document(I.buffer.document);});
+interactive("inspect-page",
+    "Inspect the current content document.",
+    function (I) { inspect_dom_document(I.buffer.document); });
 
-interactive("inspect-click", "Inspect the target of the next mouse click.",
-            function (I) {
-                var window = I.window;
-                function handler(e) {
-                    e.preventDefault();
-                    e.stopPropagation();
-                    window.removeEventListener("click", arguments.callee, true);
-                    inspect_dom_node(e.target);
-                }
-                window.addEventListener("click", handler, true);
-                I.minibuffer.message("Click in this window to select the DOM node to inspect.");
-            });
+interactive("inspect-click",
+    "Inspect the target of the next mouse click.",
+    function (I) {
+        var window = I.window;
+        function handler (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            window.removeEventListener("click", arguments.callee, true);
+            inspect_dom_node(e.target);
+        }
+        window.addEventListener("click", handler, true);
+        I.minibuffer.message("Click in this window to select the DOM node to inspect.");
+    });
