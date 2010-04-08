retroj/conkeror

commit a11243734dbf8e91c6a741da0e5da25fa06b6940

Make M-x caret-mode properly disable the caret if there is an active selection.

modules/find.js
@@ -21,9 +21,9 @@ function caret_enabled(buffer)
 }
 
 // turn on the selection in all frames
-function getFocusedSelCtrl(window)
+function getFocusedSelCtrl(buffer)
 {
-    var ds = window.buffers.current.doc_shell;
+    var ds = buffer.doc_shell;
     var dsEnum = ds.getDocShellEnumerator(Components.interfaces.nsIDocShellTreeItem.typeContent,
                                           Components.interfaces.nsIDocShell.ENUMERATE_FORWARDS);
     while (dsEnum.hasMoreElements()) {
@@ -39,14 +39,14 @@ function getFocusedSelCtrl(window)
     }
 
   // One last try
-  return window.buffers.current.doc_shell
+  return ds
       .QueryInterface(Components.interfaces.nsIInterfaceRequestor)
       .getInterface(Components.interfaces.nsISelectionDisplay)
       .QueryInterface(Components.interfaces.nsISelectionController);
 }
 
-function clear_selection(window, buffer) {
-    let sel_ctrl = getFocusedSelCtrl(window);
+function clear_selection(buffer) {
+    let sel_ctrl = getFocusedSelCtrl(buffer);
     if (sel_ctrl) {
         let sel = sel_ctrl.getSelection(sel_ctrl.SELECTION_NORMAL);
         if(caret_enabled(buffer)) {
@@ -84,7 +84,7 @@ function isearch_session(window, forward)
     this.states = [];
     this.buffer = window.buffers.current;
     this.frame = this.buffer.focused_frame;
-    this.sel_ctrl = getFocusedSelCtrl(window);
+    this.sel_ctrl = getFocusedSelCtrl(this.buffer);
     this.sel_ctrl.setDisplaySelection(Components.interfaces.nsISelectionController.SELECTION_ATTENTION);
     this.sel_ctrl.repaintSelection(Components.interfaces.nsISelectionController.SELECTION_NORMAL);
     this.states.push(new initial_isearch_state(this.buffer, this.frame, forward));

modules/bindings/default/global.js
@@ -1,4 +1,4 @@
-/**
+1/**
  * (C) Copyright 2004-2007 Shawn Betts
  * (C) Copyright 2007 John J. Foerch
  * (C) Copyright 2007-2008 Jeremy Maitin-Shepard

modules/content-buffer-input.js
@@ -135,7 +135,7 @@ add_hook("content_buffer_focus_change_hook", function (buf) { content_buffer_upd
 define_buffer_mode('caret_mode', 'CARET',
                    $enable = function(buffer) {
                        buffer.browser.setAttribute('showcaret', 'true');
-                       let sc = getFocusedSelCtrl(buffer.window);
+                       let sc = getFocusedSelCtrl(buffer);
                        let s = sc.getSelection(sc.SELECTION_NORMAL);
                        if(s.anchorNode) {
                            s.collapseToStart();
@@ -147,6 +147,7 @@ define_buffer_mode('caret_mode', 'CARET',
                        caret_input_mode(buffer, true);
                    },
                    $disable = function(buffer) {
+                       clear_selection(buffer);
                        buffer.browser.setAttribute('showcaret', '');
                        buffer.browser.focus();
                        content_buffer_update_input_mode_for_focus(buffer, true);

commit d5223e2eaffa62ec3637320b6e29f86bdf988c7e

fix issue1, issue38, issue243, issue259
it was a bug in getFocusedSelCtrl introduced in a1124373 (aug 22, 2008).
it seems to affect pages with iframe elements, when the root document had
focus.  a variable used in searching for the focused docshell gets
clobbered in the search, and inadvertently used later, when it no longer
has a valid value.

  Not 100% sure about issue38, as a full stack trace was not given, but it
seems likely enough to close that bug too.

modules/find.js
@@ -37,7 +37,7 @@ function getFocusedSelCtrl (buffer) {
     }
 
     // One last try
-    return ds
+    return buffer.doc_shell
         .QueryInterface(Ci.nsIInterfaceRequestor)
         .getInterface(Ci.nsISelectionDisplay)
         .QueryInterface(Ci.nsISelectionController);