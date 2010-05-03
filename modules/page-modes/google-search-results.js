7612048c6df188daeadd8dff19a2889672210901
John Foerch <jjfoerch@earthlink.net>
2010-05-03 19:56:34 -0400

    google-search-results: adjust results xpath expression

--- modules/page-modes/google-search-results.js
+++ modules/page-modes/google-search-results.js
@@ -25,7 +25,7 @@ define_key(google_search_results_keymap, "return", "ensure-content-focused", $fa
  */
 
 define_browser_object_class("google-search-results-links", null,
-    xpath_browser_object_handler("//a[@class='l']"),
+    xpath_browser_object_handler("//a[@class='l']|//a[@class='l vst']"),
     $hint = "select search result");
 
 
