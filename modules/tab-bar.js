retroj/conkeror

commit d3f0d0d5ce065a31579f16d82f16ab3f5b0a9a09

tab_bar.prototype.destroy delete window.tab_bar.
since the constructor installs this reference, it makes sense that the
destructor should remove it, rather than the mode's uninstall function.

modules/tab-bar.js
@@ -44,6 +44,7 @@ tab_bar.prototype.destroy = function () {
     this.window.buffers.for_each(function (b) { delete b.tab; });
     this.selected_buffer = null;
     this.element.parentNode.removeChild(this.element);
+    delete window.tab_bar;
 };
 tab_bar.prototype.update_multiple_attribute = function () {
     if (this.window.buffers.count > 1)
@@ -127,7 +128,6 @@ function tab_bar_uninstall (window) {
     if (!window.tab_bar)
         throw new Error("tab bar not initialized for window");
     window.tab_bar.destroy();
-    delete window.tab_bar;
 }
 
 define_global_mode("tab_bar_mode",

modules/new-tabs.js
@@ -72,6 +72,7 @@ tab_bar.prototype.destroy = function () {
     this.window.buffers.for_each(function (b) { delete b.tab; });
     this.selected_buffer = null;
     this.element.parentNode.removeChild(this.element);
+    delete this.window.tab_bar;
 };
 
 
@@ -221,7 +222,6 @@ function tab_bar_uninstall (window) {
     if (!window.tab_bar)
         throw new Error("tab bar not initialized for window");
     window.tab_bar.destroy();
-    delete window.tab_bar;
 }
 
 

commit bcf0d26ae957882c7f6f740bf1d4ce89e877bd32

tab-bar: fix bug introduced in d3f0d0d
'tab-bar-mode' failed to disable the tab bar because of an incorrect
reference to the window.

modules/tab-bar.js
@@ -44,7 +44,7 @@ tab_bar.prototype.destroy = function () {
     this.window.buffers.for_each(function (b) { delete b.tab; });
     this.selected_buffer = null;
     this.element.parentNode.removeChild(this.element);
-    delete window.tab_bar;
+    delete this.window.tab_bar;
 };
 tab_bar.prototype.update_multiple_attribute = function () {
     if (this.window.buffers.count > 1)