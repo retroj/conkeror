f3eadf12bca2dd2500f9a23743b20d72e0760ffa
John Foerch <jjfoerch@earthlink.net>
2011-09-06 20:27:49 -0400

    set layout.word_select.stop_at_punctuation to true
    This affects double-clicking on a word, or hitting M-back_space.  Setting
    it to true means punctuation characters are treated as word boundaries.
    This is already the default on most platforms, but we had a report that it
    was not so on OpenSUSE.

---
+++ defaults/preferences/gui.js
@@ -0,0 +1,8 @@
+/**
+ * (C) Copyright 2011 John J. Foerch
+ *
+ * Use, modification, and distribution are subject to the terms specified in the
+ * COPYING file.
+**/
+
+pref("layout.word_select.stop_at_punctuation", true);
