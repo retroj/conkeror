e46dc990c9a59d3bfd0d317b6907e7de470e2a83
John Foerch <jjfoerch@earthlink.net>
2012-02-12 15:11:17 -0500

    tests/simple/page-mode-activation.js: update a variable name

--- tests/simple/page-mode-activation.js
+++ tests/simple/page-mode-activation.js
@@ -3,13 +3,13 @@ require("walnut.js");
 
 walnut_run({
     test_youtube_1: function () {
-        assert_equals(regexp_exec(youtube_uri_regexp,
+        assert_equals(regexp_exec(youtube_mode_test,
                                   "http://www.youtube.com/watch?v=cpmnazm6HVU&feature=related",
                                   1),
                       "cpmnazm6HVU");
     },
     test_youtube_2: function () {
-        assert_equals(regexp_exec(youtube_uri_regexp,
+        assert_equals(regexp_exec(youtube_mode_test,
                                   "http://www.youtube.com/watch?feature=player_detailpage&v=6ivsFnbeM_Q",
                                   1),
                       "6ivsFnbeM_Q");
