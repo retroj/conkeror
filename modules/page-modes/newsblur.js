91359fc019c05a3b7716b66a2efd8eeec1e34c0e
Benjamin Slade <slade@jnanam.net>
2012-10-29 14:26:50 -0400

    newsblur-mode: more keymap updates

--- modules/page-modes/newsblur.js
+++ modules/page-modes/newsblur.js
@@ -1,5 +1,5 @@
 /**
- * NewBlur mode (for https://www.newsblur.com RSS news reader)
+ * NewsBlur mode (for https://www.newsblur.com RSS news reader)
  * (C) Copyright 2012 Benjamin Slade
  *
  * Use, modification, and distribution are subject to the terms specified in the
@@ -16,17 +16,20 @@ define_fallthrough(newsblur_keymap, match_any_unmodified_character);
 define_key(newsblur_keymap, "?", null, $fallthrough); // open help
 
 // Navigation
-define_key(newsblur_keymap, "d", null, $fallthrough); // dashboard
-define_key(newsblur_keymap, "g", null, $fallthrough); // find feed by name
-define_key(newsblur_keymap, "j", null, $fallthrough); // next story
-define_key(newsblur_keymap, "k", null, $fallthrough); // previous story
-define_key(newsblur_keymap, "J", null, $fallthrough); // next site
-define_key(newsblur_keymap, "S-up", null, $fallthrough); // next site
-define_key(newsblur_keymap, "K", null, $fallthrough); // previous site
-define_key(newsblur_keymap, "S-down", null, $fallthrough); // previous site
+define_key(newsblur_keymap, "d", null, $fallthrough); // back to dashboard
 define_key(newsblur_keymap, "E", null, $fallthrough); // fetch everything (fetch all unread stories)
-define_key(newsblur_keymap, "space", null, $fallthrough); // scroll down
+define_key(newsblur_keymap, "g", null, $fallthrough); // find feed by name
+define_key(newsblur_keymap, "k", null, $fallthrough); // previous story
+define_key(newsblur_keymap, "j", null, $fallthrough); // next story
+define_key(newsblur_keymap, "up", null, $fallthrough); // previous story
+define_key(newsblur_keymap, "down", null, $fallthrough); // next story
+define_key(newsblur_keymap, "K", null, $fallthrough); // previous site
+define_key(newsblur_keymap, "J", null, $fallthrough); // next site
+define_key(newsblur_keymap, "S-up", null, $fallthrough); // previous site
+define_key(newsblur_keymap, "S-down", null, $fallthrough); // next site
 define_key(newsblur_keymap, "S-space", null, $fallthrough); // scroll up
+define_key(newsblur_keymap, "space", null, $fallthrough); // scroll down
+define_key(newsblur_keymap, "p", null, $fallthrough); // previous unread story
 define_key(newsblur_keymap, "n", null, $fallthrough); // next unread story
 define_key(newsblur_keymap, "m", null, $fallthrough); // find oldest unread story
 define_key(newsblur_keymap, "left", null, $fallthrough); // switch views (original|feed|story)
