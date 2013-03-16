8c9cf4f56d7a338320e8e0a1cd95c8e989d36f14
John Foerch <jjfoerch@earthlink.net>
2013-03-16 10:24:05 -0400

    isearch_keymap: bind C-y to 'yank'

--- modules/bindings/default/isearch.js
+++ modules/bindings/default/isearch.js
@@ -17,5 +17,6 @@ define_key(isearch_keymap, "C-s",           "isearch-continue-forward");
 define_key(isearch_keymap, "C-g",           "minibuffer-abort");
 define_key(isearch_keymap, "escape",        "minibuffer-abort");
 define_key(isearch_keymap, "M-escape",      "minibuffer-abort");
+define_key(isearch_keymap, "C-y", "yank");
 
 define_key(isearch_keymap, match_any_key, "isearch-done");
