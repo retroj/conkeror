9bedf9a5d32ab22f1209e900bd851aa0e71906fe
John Foerch <jjfoerch@earthlink.net>
2013-03-16 10:32:48 -0400

    bindings/default/isearch.js: whitespace

--- modules/bindings/default/isearch.js
+++ modules/bindings/default/isearch.js
@@ -11,12 +11,12 @@ define_fallthrough(isearch_keymap, match_any_unmodified_character);
 
 define_key(isearch_keymap, match_any_unmodified_character, null, $fallthrough);
 
-define_key(isearch_keymap, "back_space",    "isearch-backspace");
-define_key(isearch_keymap, "C-r",           "isearch-continue-backward");
-define_key(isearch_keymap, "C-s",           "isearch-continue-forward");
-define_key(isearch_keymap, "C-g",           "minibuffer-abort");
-define_key(isearch_keymap, "escape",        "minibuffer-abort");
-define_key(isearch_keymap, "M-escape",      "minibuffer-abort");
+define_key(isearch_keymap, "back_space", "isearch-backspace");
+define_key(isearch_keymap, "C-r", "isearch-continue-backward");
+define_key(isearch_keymap, "C-s", "isearch-continue-forward");
+define_key(isearch_keymap, "C-g", "minibuffer-abort");
+define_key(isearch_keymap, "escape", "minibuffer-abort");
+define_key(isearch_keymap, "M-escape", "minibuffer-abort");
 define_key(isearch_keymap, "C-y", "yank");
 
 define_key(isearch_keymap, match_any_key, "isearch-done");
