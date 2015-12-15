fa0821a2550f8c740a6fa9ff63103457049c5b71
thorkill <thkr@hannover.ccc.de>
2015-12-15 12:25:25 +0100

    Fixes JavaScript warning: chrome://conkeror/content/keymap.js, line 14: JavaScript 1.7's let blocks are deprecated

--- modules/keymap.js
+++ modules/keymap.js
@@ -10,19 +10,16 @@
 /* Generate vk name table  */
 var keycode_to_vk_name = [];
 var vk_name_to_keycode = {};
-let (KeyEvent = Ci.nsIDOMKeyEvent,
-     prefix = "DOM_VK_") {
-    for (var i in KeyEvent) {
-        /* Check if this is a key binding */
-        if (i.substr(0, prefix.length) == prefix) {
-            let name = i.substr(prefix.length).toLowerCase();
-            let code = KeyEvent[i];
-            keycode_to_vk_name[code] = name;
-            vk_name_to_keycode[name] = code;
-        }
+let KeyEvent = Ci.nsIDOMKeyEvent, prefix = "DOM_VK_";
+for (var i in KeyEvent) {
+    /* Check if this is a key binding */
+    if (i.substr(0, prefix.length) == prefix) {
+        let name = i.substr(prefix.length).toLowerCase();
+        let code = KeyEvent[i];
+        keycode_to_vk_name[code] = name;
+        vk_name_to_keycode[name] = code;
     }
 }
-
 
 /*
  * Modifiers
